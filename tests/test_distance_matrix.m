18a4321ef3895bbd2fcec62cdf8e08dca2ad0d75
Julien Bect <julien.bect@supelec.fr>
2012-02-08 08:34:28 +0000

    more efficient version of stk_sampling_maximinlhs()

--- tests/test_distance_matrix.m
+++ tests/test_distance_matrix.m
@@ -1,3 +1,32 @@
+%          STK : a Small (Matlab/Octave) Toolbox for Kriging
+%
+% Copyright Notice
+%
+%    Copyright (C) 2011 SUPELEC
+%    Version:   1.0
+%    Authors:   Julien Bect        <julien.bect@supelec.fr>
+%               Emmanuel Vazquez   <emmanuel.vazquez@supelec.fr>
+%    URL:       http://sourceforge.net/projects/kriging
+%
+% Copying Permission Statement
+%
+%    This  file is  part  of  STK: a  Small  (Matlab/Octave) Toolbox  for
+%    Kriging.
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+%
+
 NB_TESTS = 3;
 TOL_REL = 1e-15;
 
