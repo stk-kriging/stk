b4737d9cf4ad8921d94775d6ea45b7a02fe3e6d2
Julien Bect <julien.bect@centralesupelec.fr>
2012-10-24 19:31:53 +0000

    Added a new function to draw shaded area representing pointwise confidence intervals.
    * misc/plot/stk_plot_shadedci.m: new function
    * misc/plot/stk_plot1d.m: modified

--- misc/plot/stk_plot_shadedci.m
+++ misc/plot/stk_plot_shadedci.m
@@ -1,10 +1,4 @@
-% STK_PLOT1D is a convenient plot function in 1D
-%
-% CALL: stk_plot1d ( xi, zi, xt, zt, zp )
-%
-% STK_PLOT1D plots the result of a 1D approximation
-%
-% EXAMPLE: See examples/example01.m
+% STK_PLOT_SHADEDCI represents pointwise confidence itervals using a shaded area
 
 % Copyright Notice
 %
@@ -33,28 +27,12 @@
 %    You should  have received a copy  of the GNU  General Public License
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
 
-function stk_plot1d(xi, zi, xt, zt, zp, h_axis)
+function stk_plot_shadedci(x, zp)
 
-if nargin < 6,
-    % Create figure
-    figure1 = figure('InvertHardcopy', 'off', 'Color', [1 1 1]);
-    % Create axes
-    h_axis = axes('Parent', figure1, 'FontSize', 12);
-end
+delta = 1.96 * sqrt(abs(zp.v));
+h = area(x.a, [zp.a - delta, 2 * delta]);
+set(h(1), 'FaceColor', 'none');
+set(h(2), 'FaceColor', [0.8 0.8 0.8]);
+set(h, 'LineStyle', '-', 'LineWidth', 1, 'EdgeColor', 'none');
 
-% shaded area representing pointwise confidence intervals
-stk_plot_shadedci(xt, zp); hold on;
-
-% ground truth (if available)
-if ~isempty(zt),
-    plot(xt.a, zt.a, '--', 'LineWidth', 3, 'Color', [0.39, 0.47, 0.64]);
-end
-
-% kriging predictor (posterior mean)
-plot(xt.a, zp.a, 'LineWidth', 4, 'Color', [0.95 0.25 0.3]);
-
-% evaluations
-plot(xi.a, zi.a, 'ks', 'MarkerSize', 10, 'LineWidth', 3, ...
-    'MarkerEdgeColor', [0.95 0.25 0.3], 'MarkerFaceColor', [0.8 0.8 0.8]);
-
-hold off; set(h_axis, 'box', 'off');
+end % stk_plot_shadedci
