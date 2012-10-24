68b046142776564edee4d5ea6822ea5b000d19e8
Julien Bect <julien.bect@supelec.fr>
2012-10-24 19:58:47 +0000

    Fixing some copyright notices.

--- misc/disp/stk_disp_framedtext.m
+++ misc/disp/stk_disp_framedtext.m
@@ -2,10 +2,9 @@
 
 % Copyright Notice
 %
-%    Copyright (C) 2011, 2012 SUPELEC
+%    Copyright (C) 2012 SUPELEC
 %
-%    Authors:   Julien Bect       <julien.bect@supelec.fr>
-%               Emmanuel Vazquez  <emmanuel.vazquez@supelec.fr>
+%    Author:  Julien Bect  <julien.bect@supelec.fr>
 %
 % Copying Permission Statement
 %
