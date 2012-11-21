3d18f6b2323985145c4767fcae9a5625c665190a
Julien Bect <julien.bect@centralesupelec.fr>
2012-11-21 16:09:03 +0000

    Added default values for some input arguments of stk_is_lhs(), and stricter dimension checking.
    * misc/test/stk_is_lhs.m: modified

--- misc/test/stk_is_lhs.m
+++ misc/test/stk_is_lhs.m
@@ -1,11 +1,29 @@
 % STK_IS_LHS tests if a given set of points forms a LHS
-
+%
+% CALL: OK = stk_is_lhs(X, N, DIM, BOX)
+%
+%    tests if X is a Latin Hypercube Sample (LHS) of size N, over the hyper-
+%    rectangle BOX of dimension DIM. The result OK is true if X is a LHS and
+%    false otherwise.
+%
+% CALL: OK = stk_is_lhs(X, N, DIM)
+%
+%    tests if X is a Latin Hypercube Sample (LHS) of size N, over the hyper-
+%    rectangle [0; 1]^DIM.
+%
+% CALL: OK = stk_is_lhs(X)
+%
+%    tests if X is a Latin Hypercube Sample (LHS). Both the size N and the 
+%    number DIM of factors are inferred from X.
+%
+% All three calling syntaxes accepts both matrix-type inputs or data structures
+% (with an 'a' field) for X.
+  
 % Copyright Notice
 %
 %    Copyright (C) 2012 SUPELEC
 %
-%    Authors:   Julien Bect        <julien.bect@supelec.fr>
-%               Emmanuel Vazquez   <emmanuel.vazquez@supelec.fr>
+%    Author:  Julien Bect  <julien.bect@supelec.fr>
 %
 % Copying Permission Statement
 %
@@ -28,6 +46,18 @@
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
 
 function b = stk_is_lhs(x, n, dim, box)
+stk_narginchk(1, 4);
+
+x = stk_datastruct(x);
+
+if nargin == 1,
+    [n dim] = size(x.a);
+else
+    if ~isequal(size(x.a), [n dim])
+        errmsg = 'Incorrect dimensions.';
+        stk_error(errmsg, 'IncorrectArgument');
+    end
+end
 
 if nargin < 4,
     box = [zeros(1, dim); ones(1, dim)];
