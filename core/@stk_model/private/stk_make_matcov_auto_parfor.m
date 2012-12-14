97488306012ba9ecdce84f3c557a9fc32fd31e95
Julien Bect <julien.bect@supelec.fr>
2012-12-14 10:37:05 +0000

    Merging changes from the trunk.

--- core/@stk_model/private/stk_make_matcov_auto_parfor.m
+++ core/@stk_model/private/stk_make_matcov_auto_parfor.m
@@ -1,19 +1,19 @@
 % STK_MAKE_MATCOV_AUTO_PARFOR [STK internal function]
 
-%                  Small (Matlab/Octave) Toolbox for Kriging
-%
 % Copyright Notice
 %
 %    Copyright (C) 2011, 2012 SUPELEC
-%    Version:   1.1
+%
 %    Authors:   Julien Bect       <julien.bect@supelec.fr>
 %               Emmanuel Vazquez  <emmanuel.vazquez@supelec.fr>
-%    URL:       http://sourceforge.net/projects/kriging/
-%
+%               Benoit Jan        <benoit.jan@supelec.fr>
+
 % Copying Permission Statement
 %
-%    This  file is  part  of  STK: a  Small  (Matlab/Octave) Toolbox  for
-%    Kriging.
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
 %
 %    STK is free software: you can redistribute it and/or modify it under
 %    the terms of the GNU General Public License as published by the Free
@@ -27,18 +27,19 @@
 %
 %    You should  have received a copy  of the GNU  General Public License
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
-%
 
 function K = stk_make_matcov_auto_parfor(kfun, x, ncores, min_block_size)
 
+if isstruct(x0), x0 = x0.a; end
+
 %=== choose the actual block size & number of blocks
 B = 0; % number of blocks of x_i's
-n = size(x.a, 1); n_min = ceil(sqrt(min_block_size));
+n = size(x, 1); n_min = ceil(sqrt(min_block_size));
 while 1, B = B + 1;
     % stop if the blocks are becoming too small
     if n/B < n_min, B = max(1,B-1); break; end
     % stop if the number of blocks is a multiple of ncores
-    if mod(B*(B+1)/2, ncores) == 0, break; end
+    if mod(B * (B + 1) / 2, ncores) == 0, break; end
 end
 block_size = ceil(n / B); % the last block can be slightly smaller than the others
 nb_blocks = B * (B + 1) / 2;
@@ -51,8 +52,8 @@ blocks = struct('i', cell(1,nb_blocks), 'j', [], 'xi', [], 'xj', [], 'K', []);
 i = 1; j = 0;
 for b = 1:nb_blocks;
     j = j+1; if(j>i), i=i+1; j=1; end
-    blocks(b).i = i; blocks(b).xi = struct('a', x.a(ind(i,1):ind(i,2),:));
-    blocks(b).j = j; blocks(b).xj = struct('a', x.a(ind(j,1):ind(j,2),:));
+    blocks(b).i = i; blocks(b).xi = x(ind(i,1):ind(i,2),:);
+    blocks(b).j = j; blocks(b).xj = x(ind(j,1):ind(j,2),:);
 end
 
 %=== process blocks
