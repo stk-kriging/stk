283a40073d1351c95593ec217713dd28f3e19155
Julien Bect <julien.bect@supelec.fr>
2012-12-15 16:48:27 +0000

    Added a second syntax to stk_setobs().
    * core/@stk_model/stk_setobs.m: modified

--- core/@stk_model/stk_setobs.m
+++ core/@stk_model/stk_setobs.m
@@ -4,6 +4,10 @@
 %
 %   returns a structure MODEL with observations set to OBS
 %
+% CALL: MODEL = stk_setobs(MODEL, X, Z)
+%
+%   returns a structure MODEL with observations set to (X, Z).
+%
 % See also stk_makedata, stk_model, ...
 
 % Copyright Notice
@@ -12,7 +16,7 @@
 %
 %    Authors:   Julien Bect       <julien.bect@supelec.fr>
 %               Emmanuel Vazquez  <emmanuel.vazquez@supelec.fr>
-%
+
 % Copying Permission Statement
 %
 %    This file is part of
@@ -32,8 +36,19 @@
 %
 %    You should  have received a copy  of the GNU  General Public License
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
-%
-function model = stk_setobs(model, xz)
+
+function model = stk_setobs(model, varargin)
+
+if nargin == 2,
+    xz = varargin{1};
+elseif nargin == 3,
+    xz = stk_makedata(varargin{1}, varargin{2});
+else
+    errmsg = 'Incorrect number of input arguments';
+    stk_err(errmsg, 'IncorrectNbInputArgs');
+end
 
 model.domain.dim   = size(xz.x.a, 2);
 model.observations = xz;
+
+end % function stk_setobs
