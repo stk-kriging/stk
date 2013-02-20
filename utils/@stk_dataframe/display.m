52f433ee420886aa67bddc91ff1fa8dfa390b46a
Julien Bect <julien.bect@supelec.fr>
2013-02-20 13:24:36 +0000

    @stk_dataframe/display.m: new method

---
+++ utils/@stk_dataframe/display.m
@@ -0,0 +1,74 @@
+% DISPLAY displays the content of a dataframe.
+%
+% Example:
+%    format short
+%    x = [1 1e6 rand; 10 -1e10 rand; 100 1e-22 rand];
+%    stk_dataframe(x)
+
+% Copyright Notice
+%
+%    Copyright (C) 2013 SUPELEC
+%
+%    Author:  Julien Bect  <julien.bect@supelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function display(x, max_width)
+
+if nargin < 2,
+    switch get(0, 'Format')
+        case 'short'
+            max_width = 6;
+        case 'long'
+            max_width = 16;
+        otherwise
+            % FIXME: handle other formatting modes...
+            max_width = 10;
+    end
+end
+
+nb_spaces_before = 1;
+nb_spaces_colsep = 2;
+
+[n, d] = size(x.data);
+
+str = repmat(' ', n + 1,  nb_spaces_before); %#ok<*AGROW>
+
+for j = 1:d,
+    
+    vn = x.vnames{j};
+    xx = stk_sprintf_colvect(x.data(:, j), max_width);   
+
+    Lxx = size(xx, 2);
+    L = max(length(vn), Lxx);
+    
+    str = [str [sprintf('% *s', L, vn);  % variable name
+        repmat(' ', n, L - Lxx) xx]];    % formatted data
+    
+    if j < d,
+        str = [str repmat(' ', n + 1,  nb_spaces_colsep)];
+    end
+    
+end % for
+
+fprintf('\n'); disp(str); fprintf('\n');
+
+end % function display
