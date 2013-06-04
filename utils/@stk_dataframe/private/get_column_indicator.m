b3fd63584b741c6bc99ee2d54fa104cb6bdb0c3c
Julien Bect <julien.bect@supelec.fr>
2013-06-04 09:02:50 +0000

    Sync merge trunk -> objectify_me (rev. 439-451).
    -----
    
    Fix two bugs related to dataframe indexing.
    
    * utils/@stk_dataframe/private/get_column_indicator.m: Fix two bugs.
    * utils/@stk_dataframe/subsref.m: Add tests.

--- utils/@stk_dataframe/private/get_column_indicator.m
+++ utils/@stk_dataframe/private/get_column_indicator.m
@@ -30,23 +30,37 @@ function b = get_column_indicator(x, s)
 
 b = strcmp(s, x.vnames);
 
+% Note: b is empty is x.vnames is empty (no column names)
+
 if ~any(b)
-    if ~strcmp(s, 'a')
-        errmsg = sprintf('There is no variable named %s.', idx(1).subs);
+    
+    if ~strcmp(s, 'a') % the special case s == 'a' is dealt with below
+        
+        errmsg = sprintf('There is no variable named %s.', s);
         stk_error(errmsg, 'UnknownVariable');
-    else
+        
+    else % special case s == 'a' (legacy feature)
+        
         b = strcmp('mean', x.vnames);
+        
         if any(b)
+            
             warning(sprintf(['There is no variable named ''a''.\n' ...
                 ' => Assuming that you''re an old STK user trying to ' ...
                 'get the kriging mean.'])); %#ok<WNTAG,SPWRN>
+            
         else
+            
             warning(sprintf(['There is no variable named ''a''.\n' ...
                 ' => Assuming that you''re an old STK user trying to ' ...
                 'get the entire dataframe.'])); %#ok<WNTAG,SPWRN>
-            b = true(size(b));
+            
+            b = true(1, size(x, 2));
+            
         end
+        
     end
+    
 end
 
 end % function get_column_indicator
