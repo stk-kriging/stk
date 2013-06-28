0e21411713c394102af23b29502f438806755b4c
Julien Bect <julien.bect@centralesupelec.fr>
2013-06-28 11:50:13 +0000

    Encapsulate kriging equations in a dedicated class.
    * core/@stk_kriging_equation: A new class that encapsulates the
     basic linear-algebraic operations related to the kriging equations.
     Methods: stk_kriging_equation.m, linsolve.m, stk_posterior_matcov.m
     and subsref.m.
    * core/stk_predict.m: Use the new class.

---
+++ core/@stk_kriging_equation/stk_kriging_equation.m
@@ -0,0 +1,47 @@
+% STK_KRIGING_EQUATION...
+
+% Copyright Notice
+%
+%    Copyright (C) 2013 SUPELEC
+%
+%    Authors:  Julien Bect       <julien.bect@supelec.fr>
+%              Emmanuel Vazquez  <emmanuel.vazquez@supelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function kreq = stk_kriging_equation(model, xi)
+
+[Kii, Pi] = stk_make_matcov(model, xi);
+
+LS =                             ...
+    [[ Kii, Pi                ]; ...
+    [  Pi', zeros(size(Pi,2)) ]];
+
+[Q, R] = qr(LS); % orthogonal-triangular decomposition
+
+kreq = struct(  'model',     model, ...
+    'xi',   xi, 'xt',        [],    ...
+    'LS_Q', Q,  'LS_R',      R,     ...
+    'RS',   [], 'lambda_mu', []     );
+
+kreq = class(kreq, 'stk_kriging_equation');
+
+end
