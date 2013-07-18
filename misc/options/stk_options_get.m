a3f9591897bfe8d9cd367367d85305b6858aed3f
Julien Bect <julien.bect@centralesupelec.fr>
2013-07-18 11:30:56 +0000

    Reintegrate merge branches/parallel -> trunk
    ---
    
    Benchmarks for parallelization.
    
    * misc/benchmarks: New folder.
    * misc/benchmarks/stk_benchmark_cov2.m: New script.
    * misc/benchmarks/stk_benchmark_cov1.m: New script.
    
    Rewrite parallelization of covariance evaluation.
    
    * core/private: Removed.
    * misc/specfun/stk_sf_matern.m: Modified (parallelized)
    
    Add wrapper classes for parallel computation engines.
    
    * misc/parallel: New folder.
    * misc/parallel/@stk_parallel_engine_none: New class.
    * misc/parallel/@stk_parallel_engine_parfor: New class.
    * misc/parallel/stk_parallel_cutblocks.m: New function.
    * misc/parallel/stk_parallel_feval.m: New function.
    * misc/parallel/stk_parallel_start.m: New function.
    * misc/parallel/stk_parallel_engine_set.m: New function.
    * misc/parallel/stk_parallel_engine_get.m: New function.
    * misc/parallel/stk_parallel_stop.m: New function.
    * misc/config/stk_path.m: Modified.
    
    A new system for managing options.
    
    * misc/options: New folder.
    * misc/options/stk_options_set.m: New function.
    * misc/options/stk_options_get.m: New function.
    * misc/config/stk_path.m: Modified.

---
+++ misc/options/stk_options_get.m
@@ -0,0 +1,49 @@
+% STK_OPTIONS_GET ... (FIXME: missing doc)
+
+% Copyright Notice
+%
+%    Copyright (C) 2013 SUPELEC
+%
+%    Author:  Julien Bect  <julien.bect@supelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function argout = stk_options_get(varargin)
+
+opts = stk_options_set();
+
+switch nargin
+    
+    case 0, % nothing to do, just return the output
+        argout = opts;
+        
+    case 1,
+        argout = opts.(varargin{1});
+        
+    case 2,
+        argout = opts.(varargin{1}).(varargin{2});
+        
+    otherwise
+        stk_error('Too many input arguments.', 'TooManyInputArgs');
+        
+end
+
+end % function stk_options_get
