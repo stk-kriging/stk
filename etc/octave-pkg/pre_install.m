97c7ee11746897cef72c8a2b1981a1411c864f7c
Julien Bect <julien.bect@supelec.fr>
2014-02-23 13:18:35 +0100

    Add load/unload scripts for the MOLE.
    * misc/mole/PKG_ADD.m: New initialization script, that adds
      selected subdirectories of the MOLE to the path.
    * misc/mole/PKG_DEL.m: Remove MOLE subdirectories from the path.
    * config/stk_config_path.m: Remove the part that is now handled by
      the new script (misc/mole/PKG_ADD.m).
    * etc/octave-pkg/pre_install.m: Run misc/mole/PKG_ADD.m first and
      PKG_DEL.m at the end (otherwise the load path would contain
      deleted temp directories after the installation).
    * stk_init.m: Run misc/mole/PKG_ADD.m first.

--- etc/octave-pkg/pre_install.m
+++ etc/octave-pkg/pre_install.m
@@ -1,10 +1,13 @@
 function pre_install (desc)
 
-  ## compile all MEX-files
   pkg_dir = pwd;
   src_dir = fullfile (pkg_dir, "src");
   inst_dir = fullfile (pkg_dir, "inst");
-  cd (inst_dir);
+
+  run (fullfile (inst_dir, 'misc', 'mole', 'PKG_ADD.m'));
+  
+  ## compile all MEX-files
+  cd (inst_dir);  
   stk_build (true, src_dir, inst_dir);
   
   ## copy AUTHORS and README to inst (this makes them available
@@ -12,7 +15,9 @@ function pre_install (desc)
   cd (pkg_dir);
   copyfile (fullfile (pkg_dir, "AUTHORS"), inst_dir);
   copyfile (fullfile (pkg_dir, "README"), inst_dir);
-  
+
+  run (fullfile (inst_dir, 'misc', 'mole', 'PKG_DEL.m'));
+
 endfunction
 
 
