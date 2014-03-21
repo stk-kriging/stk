89c219371ef5998108145f29129a6341689212c7
Julien Bect <julien.bect@supelec.fr>
2014-03-21 21:51:25 +0100

    stk_param_relik: Fix help text.
    * paramestim/stk_param_relik.m: Fix help text.

--- paramestim/stk_param_relik.m
+++ paramestim/stk_param_relik.m
@@ -1,4 +1,4 @@
-% STK_REMLQRG computes the restricted likelihood of a model given data.
+% STK_PARAM_RELIK computes the restricted likelihood of a model given data.
 %
 % CALL: [ARL, dARL_dtheta, dARL_dLNV] = stk_param_relik (MODEL, XI, YI)
 %
@@ -12,7 +12,7 @@
 
 % Copyright Notice
 %
-%    Copyright (C) 2011-2013 SUPELEC
+%    Copyright (C) 2011-2014 SUPELEC
 %
 %    Authors:   Julien Bect       <julien.bect@supelec.fr>
 %               Emmanuel Vazquez  <emmanuel.vazquez@supelec.fr>
