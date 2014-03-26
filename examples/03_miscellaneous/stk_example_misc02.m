3903ad09c6ffe1fbd455be7344508e10f09f74bc
Julien Bect <julien.bect@supelec.fr>
2014-03-26 16:41:55 +0100

    Rename stk_config_graphicstoolkit tp graphics_toolkit.
    * config/stk_config_graphicstoolkit.m: Renamed.
    * misc/mole/graphics_toolkit/graphics_toolkit.m: Formerly known as
      stk_config_graphicstoolkit. Modified to be a (partial) replacement
      of the Octave function with the same name.
    * misc/mole/init.m: Add graphics_toolkit to the path only when
      needed, i.e., when using Matlab or an old version of Octave.
    * examples/03_miscellaneous/stk_example_misc02.m: Modified.
    (grafted from 649c9d7d8eb865a87d6d034eb5b897eb93617974)

--- examples/03_miscellaneous/stk_example_misc02.m
+++ examples/03_miscellaneous/stk_example_misc02.m
@@ -128,7 +128,7 @@ for j = 1:3,
     
 end
 
-if ~ strcmp (stk_config_graphicstoolkit (), 'octave-gnuplot')
+if ~ strcmp (graphics_toolkit (), 'gnuplot')
     h1 = legend ('MAP estimates', 'REML estimate', 'mode of the prior');
     h2 = stk_subplot (2, 2, 4);  axis off;
     set (h1, 'Position', get (h2, 'Position'));
