a0e6d51ef44dfc134bee1e466c79d9d51d0087f7
Julien Bect <julien.bect@supelec.fr>
2014-07-01 08:48:49 +0200

    stk_isdominated.m: New function, returns true for dominated rows.
    * arrays/generic/stk_isdominated.m: New function
    * arrays/generic/private/stk_isdominated_mex.c: New MEX-function
    * misc/include/pareto.h: Add new function is_dominated
    * config/stk_config_makeinfo.m: Compile the new MEX-function
    * admin/octave-pkg/Makefile: Compile the new MEX-function
    * arrays/generic/private/stk_paretofind_mex.c: Modified
    * arrays/generic/stk_paretofind.m: Modified
    (grafted from 34e35e0f9644d7ec29050222b8963438a5ebdc26)

--- misc/include/pareto.h
+++ misc/include/pareto.h
@@ -33,49 +33,66 @@
 #ifndef ___LEXICAL_SORT_H___
 #define ___LEXICAL_SORT_H___
 
-/* assumes that type SCALAR is defined */
+/* assumes that types SCALAR and LOGICAL are defined */
 
 struct T {
-  int pos;
-  int stride;
-  int dim;
-  SCALAR *data;
+    int pos;
+    int stride;
+    int dim;
+    const SCALAR *data;
 };
 
 
 /*--------------------------------------------------------------*/
 
-void lexical_sort (SCALAR *x, int *idx, int n, int d);
-int pareto_find (SCALAR *x, int *idx1, int* idx2, int n, int d);
+void lexical_sort
+(SCALAR *x, int *idx, int n, int d);
 
-int lexical_cmp (const void *_x, const void *_y);
-int is_dominated (const struct T *x, const struct T *y);
-void init_tab (SCALAR *x, struct T *tab, int n, int d);
+int pareto_find
+(SCALAR *x, int *idx1, int* idx2, int n, int d);
+
+void is_dominated
+(const SCALAR *xa, const SCALAR *xb, LOGICAL *isdom,
+ int *drank, int n, int k, int d);
+
+int __lexical_cmp__
+(const void *_x, const void *_y);
+
+int __is_dominated__
+(const struct T *x, const struct T *y);
+
+void __init_tab__
+(const SCALAR *x, struct T *tab, int n, int d);
 
 
 /*--------------------------------------------------------------*/
 
-int lexical_cmp (const void *_x, const void *_y)
+int __lexical_cmp__ (const void *_x, const void *_y)
 {
-  int j, n, d;  SCALAR *px, *py;  const struct T *x, *y;
+    int j, n, d;
+    const SCALAR *px, *py;
+    const struct T *x, *y;
 
-  /* Cast to const struct T* */
-  x = (const struct T*) _x;  y = (const struct T*) _y;
+    /* Cast to const struct T* */
+    x = (const struct T*) _x;
+    y = (const struct T*) _y;
 
-  /* Pointers to the actual data */
-  px = x->data;  py = y->data;
+    /* Pointers to the actual data */
+    px = x->data;
+    py = y->data;
 
-  /* We ASSUME that x and y have the same dim and stride */
-  d = x->dim;  n = x->stride;
+    /* We ASSUME that x and y have the same dim and stride */
+    d = x->dim;
+    n = x->stride;
 
-  for (j = 0; j < d; j ++, px += n, py += n) {
-    if ((*px) < (*py))
-      return -1;  /* x < y */
-    else if ((*px) > (*py))
-      return +1;  /* y > x */
-  }
-  
-  return 0; /* x == y */
+    for (j = 0; j < d; j ++, px += n, py += n) {
+        if ((*px) < (*py))
+            return -1;  /* x < y */
+        else if ((*px) > (*py))
+            return +1;  /* y > x */
+    }
+
+    return 0; /* x == y */
 }
 
 /*--------------------------------------------------------------*/
@@ -83,27 +100,30 @@ int lexical_cmp (const void *_x, const void *_y)
 /* Is x dominated by y ?     */
 /*   note: SMALLER is better */
 
-int is_dominated (const struct T *x, const struct T *y)
+int __is_dominated__ (const struct T *x, const struct T *y)
 {
-  int j, n, d, any_xgty;  SCALAR *px, *py;
+    int j, nx, ny, d, any_xgty;
+    const SCALAR *px, *py;
 
-  /* Pointers to the actual data */
-  px = x->data;  py = y->data;
+    /* Pointers to the actual data */
+    px = x->data;
+    py = y->data;
 
-  /* We ASSUME that x and y have the same dim and stride */
-  d = x->dim;  n = x->stride;
+    d = x->dim;
+    nx = x->stride;
+    ny = y->stride;
 
-  any_xgty = 0;  /* flag: 1 if we have seen at least one component
+    any_xgty = 0;  /* flag: 1 if we have seen at least one component
                           in y that is strictly better (smaller) */
 
-  for (j = 0; j < d; j ++, px += n, py += n) {
-    if ((*px) < (*py))
-      return 0;  /* y does not dominate x */
-    else if ((*px) > (*py))
-      any_xgty = 1;
-  }
-  
-  return any_xgty; 
+    for (j = 0; j < d; j ++, px += nx, py += ny) {
+        if ((*px) < (*py))
+            return 0;  /* y does not dominate x */
+        else if ((*px) > (*py))
+            any_xgty = 1;
+    }
+
+    return any_xgty;
 }
 
 /*--------------------------------------------------------------*/
@@ -112,100 +132,161 @@ int is_dominated (const struct T *x, const struct T *y)
 
 void lexical_sort (SCALAR *x, int *idx, int n, int d)
 {
-  struct T *tab;  int i;
+    struct T *tab;
+    int i;
 
-  /* Prepare tab for sorting */
-  tab = (struct T *) malloc (n * sizeof (struct T));
-  init_tab (x, tab, n, d);
+    /* Prepare tab for sorting */
+    tab = (struct T *) malloc (n * sizeof (struct T));
+    __init_tab__ (x, tab, n, d);
 
-  /* Sort using qsort() from the standard library */
-  qsort (tab, n, sizeof (struct T), lexical_cmp);
+    /* Sort using qsort() from the standard library */
+    qsort (tab, n, sizeof (struct T), __lexical_cmp__);
 
-  /* Fill output list */
-  for (i = 0; i < n; i++)
-    idx[i] = tab[i].pos;
+    /* Fill output list */
+    for (i = 0; i < n; i++)
+        idx[i] = tab[i].pos;
 
-  free (tab);
+    free (tab);
 }
 
 /*--------------------------------------------------------------*/
 
 /* TODO: templated versions, to handle integer types, single, etc. */
 
-int pareto_find (SCALAR *x, int *idx1, int* idx2, int n, int d)
+int pareto_find (SCALAR *x, int *ndpos, int *drank, int n, int d)
 {
-  /* idx1: liste de taille n
-   *
-   *       idx1[i] = position in x of the i^th non-dominated point
-   *                 (i^th in the lexical order)  0 <= i < k
-   *
-   *       idx1[i] = -1   for all i > k
-   *
-   * idx2: another list of length n
-   * 
-   *       idx2[i] = rank (in the lexical order, between 0 and k) of
-   *                 the first dominating point if x[i, :] is dominated
-   *
-   *       idx2[i] = -1 if x[i, :] is not dominated
-   *
-   * Both lists are assumed to have been allocated by the caller.
-   *
-   * Note: all indices are 0-based in this function.   
-   */
-    
-  struct T *tab;  int i, ii, k, pos;
+    /* ndpos: liste de taille n
+     *
+     *       ndpos[i] = position in x of the i^th non-dominated point
+     *                  (i^th in the lexical order)  0 <= i < k
+     *
+     *       ndpos[i] = -1   for all i > k
+     *
+     * drank: another list of length n
+     *
+     *       drank[i] = rank (in the lexical order, between 0 and k) of
+     *                  the first dominating point if x[i, :] is dominated
+     *
+     *       drank[i] = -1 if x[i, :] is not dominated
+     *
+     * Both lists are assumed to have been allocated by the caller.
+     *
+     * Note: all indices are 0-based in this function.
+     */
 
-  /* Prepare tab for sorting */
-  tab = (struct T *) malloc (n * sizeof (struct T));
-  init_tab (x, tab, n, d);
+    struct T *tab;
+    int i, ii, k, pos;
 
-  /* Sort using qsort() from the standard library */
-  qsort (tab, n, sizeof (struct T), lexical_cmp);
+    /* Prepare tab for sorting */
+    tab = (struct T *) malloc (n * sizeof (struct T));
+    __init_tab__ (x, tab, n, d);
 
-  /* Fill idx1 and idx2 with -1 */
-  for (i = 0; i < n; i++) {
-      idx1[i] = -1;  idx2[i] = -1; }
-  
-  /* Remove dominated points                                  */
-  /*   i : rank of current point. Start at the SECOND element */  
-  for (i = 1; i < n; i++)
-    for (ii = 0; ii < i; ii++)
-      if ((tab[ii].pos >= 0) && (is_dominated (tab + i, tab + ii))) {
-          idx2[tab[i].pos] = ii;  tab[i].pos = - 1;  break; }
+    /* Sort using qsort() from the standard library */
+    qsort (tab, n, sizeof (struct T), __lexical_cmp__);
 
-  /* Note: at this stage, idx2 contains the rank of the first 
-   *       dominating point in the FULL (sorted) list of points */
+    /* Fill ndpos and drank with -1 */
+    for (i = 0; i < n; i++) {
+        ndpos[i] = -1;
+        drank[i] = -1;
+    }
 
-  /* Fill idx1 */
-  k = 0;
-  for (i = 0; i < n; i++) {
-      pos = tab[i].pos;
-      if (pos >= 0) {  /* indicates a non-dominated point */
-          idx1[k] = pos;  tab[i].pos = k;  k ++; }}
-  
-  /* Adjust idx2 to the final list of non-dominated points */
-  for (i = 0; i < n; i++)
-      if (idx2[i] != -1 )
-          idx2[i] = tab[idx2[i]].pos;
-   
-  free (tab);
-  
-  return k;
+    /* Remove dominated points                                  */
+    /*   i : rank of current point. Start at the SECOND element */
+    for (i = 1; i < n; i++)
+        for (ii = 0; ii < i; ii++)
+            if ((tab[ii].pos >= 0) &&
+                    (__is_dominated__ (tab + i, tab + ii))) {
+                drank[tab[i].pos] = ii;
+                tab[i].pos = - 1;
+                break;
+            }
+
+    /* Note: at this stage, drank contains the rank of the first
+     *       dominating point in the FULL (sorted) list of points */
+
+    /* Fill ndpos */
+    k = 0;
+    for (i = 0; i < n; i++) {
+        pos = tab[i].pos;
+        if (pos >= 0) {  /* indicates a non-dominated point */
+            ndpos[k] = pos;
+            tab[i].pos = k;
+            k ++;
+        }
+    }
+
+    /* Adjust drank to the final list of non-dominated points */
+    for (i = 0; i < n; i++)
+        if (drank[i] != -1 )
+            drank[i] = tab[drank[i]].pos;
+
+    free (tab);
+
+    return k;
 }
 
 /*--------------------------------------------------------------*/
 
-void init_tab (SCALAR *x, struct T *tab, int n, int d)
+void is_dominated (const SCALAR *xa, const SCALAR *xb,
+                   LOGICAL *isdom, int *drank,
+                   int n, int k, int d)
 {
-  int i;
+    int i, j, b;
+    struct T T1, T2;
+    SCALAR xa1;
 
-  /* Fill in array to be sorted */
-  for (i = 0; i < n; i++) {
-    tab[i].pos   = i;
-    tab[i].data   = x + i;
-    tab[i].dim    = d;
-    tab[i].stride = n;
-  }
+    T1.pos = 0; /* unused */
+    T1.stride = n;
+    T1.dim = d;
+
+    T2.pos = 0; /* unused */
+    T2.stride = k;
+    T2.dim = d;
+
+    for (i = 0; i < n; i++) {
+
+        b = 0;             /* not dominated, until proved otherwise */
+        T1.data = xa + i;  /* pointer to the current row            */
+        xa1 = xa[i];       /* first element of the current row      */
+
+        for (j = 0; j < k; j++) {
+
+            /* xb is assumed to be LEXICALLY SORTED => as soon as the
+            first element becomes bigger than xa1, we know that
+             the current row is not dominated */
+            if (xb[j] > xa1)
+                break;
+
+            T2.data = xb + j;
+            if (__is_dominated__ (&T1, &T2)) {
+                b = 1;
+                break;
+            }
+        }
+
+        if (b) {
+            drank[i] = j;
+            isdom[i] = (LOGICAL) 1;
+        } else {
+            drank[i] = -1;
+            isdom[i] = (LOGICAL) 0;
+        }
+    }
+}
+
+/*--------------------------------------------------------------*/
+
+void __init_tab__ (const SCALAR *x, struct T *tab, int n, int d)
+{
+    int i;
+
+    /* Fill in array to be sorted */
+    for (i = 0; i < n; i++) {
+        tab[i].pos   = i;
+        tab[i].data   = x + i;
+        tab[i].dim    = d;
+        tab[i].stride = n;
+    }
 
 }
 
