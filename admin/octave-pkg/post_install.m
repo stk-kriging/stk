65dce10890b2804ce445ca8eeae782d246616906
Julien Bect <julien.bect@supelec.fr>
2014-07-21 22:03:11 +0200

    post_install.m: Add comments (design notes).

--- admin/octave-pkg/post_install.m
+++ admin/octave-pkg/post_install.m
@@ -2,15 +2,32 @@ function post_install (desc)
 
   root = desc.dir;
   config = fullfile (root, "config");
-  
+
+  # Prune unused functions from the MOLE
   addpath (config);
   stk_config_mole (root, false, true);
   rmpath (config);
 
+  # PKG_ADD/PKG_DEL (see design notes below)
   movefile (fullfile (root, "PKG_ADD.m"), ...
 	    fullfile (root, "PKG_ADD"));
-
   movefile (fullfile (root, "PKG_DEL.m"), ...
 	    fullfile (root, "PKG_DEL"));
 
 endfunction
+
+
+# ~~~~~ DESIGN NOTES ~~~~~
+# 
+#   The following approaches didn't work:
+#
+#   1) PKG_ADD/PKG_DEL (without .m) at the package root
+#
+#      The files are copied to the arch-dependent directory,
+#      which is not where we want them to be.
+#
+#   2) PKG_ADD/PKG_DEL (without .m) in ./inst
+#
+#      The files are not copied at all. Pity.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~
