497911aca4fc051f0bd92ee4026dc5f7847bd0f3
Julien Bect <julien.bect@centralesupelec.fr>
2014-08-06 18:11:42 +0200

    Define list of field names for stk_factorialdesign objects.
    * arrays/@stk_factorialdesign/fieldnames.m: Define list of field
      names for stk_factorialdesign objects.

---
+++ arrays/@stk_factorialdesign/fieldnames.m
@@ -0,0 +1,40 @@
+% FIELDNAMES [overloaded base function]
+
+% Copyright Notice
+%
+%    Copyright (C) 2014 SUPELEC
+%
+%    Author:  Julien Bect  <julien.bect@supelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function fn = fieldnames (x)
+
+fn = fieldnames (x.stk_dataframe);
+fn = ['levels' fn];
+
+end % function fieldnames
+
+%!test
+%! x = stk_factorialdesign ({0:1, 3:5}, {'u' 'v'})
+%! fn1 = sort (fieldnames (x));
+%! fn2 = {'colnames', 'data', 'info', 'levels', 'rownames', 'u', 'v'};
+%! assert (isequal (fn1, fn2));
