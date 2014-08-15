0a2d8d42f0e106d450430e9f4db89d2318fe1c71
Julien Bect <julien.bect@centralesupelec.fr>
2014-08-15 09:46:42 +0200

    @stk_dataframe/plot.m: Full rewrite.
    * arrays/@stk_dataframe/plot.m: Full rewrite to improve
      compatibility with the base plot function. Deprecate the case
      where x is an stk_dataframe objects with two or more columns.

--- arrays/@stk_dataframe/plot.m
+++ arrays/@stk_dataframe/plot.m
@@ -26,75 +26,132 @@
 %    You should  have received a copy  of the GNU  General Public License
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
 
-function plot (x, varargin)
+function plot (arg1, varargin)
 
-if ~ strcmp (x, 'stk_dataframe'),
-    if isa (x, 'stk_dataframe')
-        x = stk_dataframe (double (x), get (x, 'colnames'));
-    else
-        x = stk_dataframe (double (x));
-    end
+%--- Plot is highly polymorphic... which case are we dealing with ? -------
+
+% Extract "numeric" arguments from varargin
+%    (note: axes handles ARE numeric argument... WTF...)
+
+T = @(a)(isnumeric (a) || isa (a, 'stk_dataframe'));
+
+if ~ T (arg1)    
+    stk_error (['The first argument to stk_dataframe/plot must be ' ...
+        'numeric or an stk_dataframe object.'], 'TypeMismatch');
 end
-
+k = 1;  % at this point, we have at least one "numeric" argument
 if (nargin > 1) && (~ ischar (varargin{1}))
-    if size(x, 2) > 1,
-        stk_error('Incorrect size for argument x.', 'IncorrectSize');
-    else
-        z = varargin{1};
-        n = size (x, 1);
-        if size (z, 1) == n,
-            x = horzcat (x, z);
-        elseif isequal(size (z), [1 n]),
-            % special case: we tolerate that in silence...
-            x = horzcat (x, z');
+    arg2 = varargin{1};  varargin(1) = [];
+    if ~ T (arg2)
+        stk_error (['The second argument to stk_dataframe/plot must be' ...
+            ' numeric or an stk_dataframe object.'], 'TypeMismatch');
+    end
+    k = 2;  % at this point, we have at least two "numeric" arguments
+    if (nargin > 2) && (~ ischar (varargin{1}))
+        arg3 = varargin{1};  varargin(1) = [];
+        if ~ T (arg3)
+            stk_error (['The third argument to stk_dataframe/plot ' ...
+                'must be numeric or an stk_dataframe object.'], ...
+                'TypeMismatch');
         end
-        opts_pos = 2;
+        k = 3;
     end
-else
-    if size (x, 2) > 2,
-        warning ('Plotting the first two columns only.'); %#ok<WNTAG>
-        % TODO: implement scatter plot matrices
+    if (nargin > 3) && (~ ischar (varargin{1}))
+        stk_error (['Syntax error, expecting a char argument at the ' ...
+            'fourth position.'], 'TypeMismatch');
     end
-    opts_pos = 1;
+end 
+
+% Now figure out which case we are dealing with
+
+switch k
+    case 1
+        z = arg1;
+        h = gca;                 % default: current axes
+        x = (1:(size (z, 1)))';  % default: x = 1 ... n
+    case 2
+        arg1_handle = false;
+        if isnumeric (arg1) && isscalar (arg1)  % perhaps a handle ?
+            try  arg1_handle = strcmp (get (arg1, 'Type'), 'axes');  end
+        end
+        if arg1_handle,
+            h = arg1;
+            z = arg2;
+            x = (1:(size (z, 1)))';  % default: x = 1 ... n
+        else
+            x = arg1;
+            z = arg2;
+            h = gca;
+        end
+    case 3
+        try
+            assert (strcmp (get (arg1, 'Type'), 'axes'));
+        catch
+            stk_error (['When calling stk_dataframe/plot with three ' ...
+                'numeric arguments, the first one must be a handle to ' ...
+                'an axes object handle.'], 'IncorrectArgument');
+        end
+        h = arg1;
+        x = arg2;
+        z = arg3;
 end
 
-if nargin > opts_pos,
-    opts = varargin(opts_pos:end);
+%--- Handle stk_dataframe inputs ------------------------------------------
+
+if isa (x, 'stk_dataframe'),
+    xlab = x.colnames;
+    x = double (x);    
 else
-    opts = {};
+    xlab = {};
 end
 
-xx = double (x);
-yy = xx(:, 2:end);
-xx = xx(:, 1);
-
-nb_outputs = size (yy, 2);
-
-colnames = get (x, 'colnames');
-
-if ~ isempty (colnames)
-    xlab = colnames{1};
-    ylab = colnames(2:end);
+if isa (z, 'stk_dataframe'),
+    zlab = z.colnames;
+    z = double (z);    
 else
-    xlab = '';
-    ylab = {};
+    zlab = {};
 end
 
-plot (xx, yy, opts{:});
+%--- Dimension of x and z ? -----------------------------------------------
+
+% Tolerate row vector for x
+if isrow (x),  x = x';  end
+
+% Handle deprecated syntax where x has several columns
+if size (x, 2) > 1
+    warning ('STK:plot:Deprecated', sprintf (['DEPRECATED SYNTAX\n' ...
+        'stk_dataframe/plot (x, ...) not supported anymore for ' ...
+        'stk_dataframe objects with more than one column.']));
+    plot (x.data(:, 1), x.data(:, 2), varargin{:});
+    return;
+end
+
+% Number of points
+n = size (x, 1);
+
+% Tolerate row vector for z
+if isequal (size (z), [1 n]),  z = z';  end
+        
+%--- Plot and set labels --------------------------------------------------
+
+plot (h, x, z, varargin{:});
 
 if ~ isempty (xlab),
     stk_xlabel (xlab);
 end
 
-if ~ isempty (ylab)
-    if nb_outputs == 1,
-        stk_ylabel (ylab{1});
+if ~ isempty (zlab)
+    if size (z, 2) == 1,
+        stk_ylabel (zlab{1});
     else
-        legend (ylab{:});
+        legend (zlab{:});
     end
 end
 
 end % function plot
+
+%#ok<*SPWRN,*TRYNC>
+
 
 %!test % plot with x as a vector and z as a (univariate) dataframe
 %! x = linspace(0, 2*pi, 30)';
@@ -110,16 +167,3 @@ end % function plot
 %! x = stk_dataframe(linspace(0, 2*pi, 30)');
 %! z = sin(double(x));
 %! figure; plot(x, z); close(gcf);
-
-%!test
-%! x = stk_dataframe (rand (10, 2));
-%! figure; plot (x); close (gcf);
-
-%!test % same thing, but with more than two columns
-%! x = stk_dataframe (rand (10, 4));
-%! figure; plot (x, 'k.'); close (gcf);
-
-%!error % the first argument should have one and only one column
-%! x = stk_dataframe(rand(10, 2));
-%! z = stk_dataframe(rand(10, 1));
-%! plot(x, z);
