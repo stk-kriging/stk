0ebf320a0104e139615460c7b9a80f1d2c843af0
Julien Bect <julien.bect@centralesupelec.fr>
2014-12-28 14:57:23 +0100

    Improve stk_xlabel, stk_ylabel, and add stk_zlabel.

--- misc/options/stk_options_set.m
+++ misc/options/stk_options_set.m
@@ -80,6 +80,7 @@ opts.stk_param_estim.optim_display_level = 'off';
 opts.stk_figure.properties = {'InvertHardcopy', 'off', 'Color', [1 1 1]};
 opts.stk_xlabel.properties = {'FontSize', 10, 'Color', [0.2 0 1]};
 opts.stk_ylabel.properties = opts.stk_xlabel.properties;
+opts.stk_zlabel.properties = opts.stk_xlabel.properties;
 opts.stk_title.properties = {'FontSize', 10, 'FontWeight', 'bold'};
 opts.stk_axes.properties = {'FontSize', 8};
   
--- misc/plot/stk_get_axis_arg.m
+++ misc/plot/stk_get_axis_arg.m
@@ -1,8 +1,8 @@
-% STK_XLABEL is a replacement for 'xlabel' for use in STK's examples
+% STK_GET_AXIS_ARG [STK internal]
 
 % Copyright Notice
 %
-%    Copyright (C) 2013, 2014 SUPELEC
+%    Copyright (C) 2014 SUPELEC
 %
 %    Author:  Julien Bect  <julien.bect@supelec.fr>
 
@@ -26,26 +26,21 @@
 %    You should  have received a copy  of the GNU  General Public License
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
 
-function h = stk_xlabel (varargin)
+function [h, argin, n_argin] = stk_get_axis_arg (varargin)
 
-[h, varargin] = stk_get_axis_arg (varargin{:});
-xlab = varargin{1};
-
-% Get global STK options
-stk_options = stk_options_get ('stk_xlabel', 'properties');
-user_options = varargin(2:end);
-
-% Apply to all axes
-for i = 1:(length (h))
-    
-    % Set x-label and apply STK options
-    xlabel (h(i), xlab, stk_options{:});
-    
-    % Apply user-provided options
-    if ~ isempty (user_options)
-        set (h(i), user_options{:});
+if isempty (varargin)
+    h = [];
+    argin = {};
+else
+    h = varargin{1};
+    if (all (ishghandle (h))) && (all (strcmp (get (h, 'type'), 'axes')))
+        argin = varargin(2:end);
+    else
+        h = gca;
+        argin = varargin;
     end
-    
 end
 
-end % function stk_xlabel
+n_argin = length (argin);
+
+end % function stk_get_axis_arg
