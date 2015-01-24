68df8ad9a42b87082fd7d1d247df209439f7ee51
Julien Bect <julien.bect@centralesupelec.fr>
2015-01-24 19:20:57 +0100

    view_samplingcrit_1d.m: Do not assume that xinew is an stk_dataframe object

--- iago/viewfcs/view_samplingcrit_1d.m
+++ iago/viewfcs/view_samplingcrit_1d.m
@@ -66,7 +66,7 @@ end
 for i = 1:nplots
     figure(h_fig)
     h = subplot(nplots, 1, i); hold on;
-    plot([xinew.data xinew.data], ylim(h), 'LineWidth', 2, 'Color', [0.6, 0.6, 0.6])
+    plot(xinew * [1 1], ylim(h), 'LineWidth', 2, 'Color', [0.6, 0.6, 0.6])
     hold off;
 end
 
