103bb51068d3ec43f1ad4c3333eedb739ff5aca0
Julien Bect <julien.bect@centralesupelec.fr>
2015-02-01 09:21:56 +0100

    stk_optim_crit_iago.m: Fix CEM criterion in the noisy case

--- iago/stk_optim_crit_iago.m
+++ iago/stk_optim_crit_iago.m
@@ -46,7 +46,7 @@ assert (noise_params_consistency (algo, xi));
 
 %% SIMULATION + INITIAL PREDICTION
 
-ni = stk_length (xi);  xi_ind = 1:ni;
+ni = stk_length (xi);  xi_ind = (1:ni)';
 
 xc = algo.xg0;  nc = stk_length (xc);  % candidate points
 xg = [xi; xc];  ng = ni + nc;          % evaluations points & candidate points
@@ -55,11 +55,40 @@ if algo.disp,  view_init (algo, xi, zi, xg);  end
 
 model_xg = stk_model ('stk_discretecov', algo.model, xg);
 [model_xg, zi] = stk_fakenorep (model_xg, zi);
-zsim = stk_generate_samplepaths (model_xg, (1:ng)', algo.nsamplepaths);
+z_sim0 = stk_generate_samplepaths (model_xg, (1:ng)', algo.nsamplepaths);
 [zp, lambda] = stk_predict (model_xg, xi_ind, zi, []);
 
 
+%% Compute current entropy
+
+% Simulate observation noise
+%  (note: this should be done by stk_generate_samplepaths)
+if any (model_xg.lognoisevariance ~= -inf)
+    noise_sim = bsxfun (@times, ...
+        exp (0.5 * model_xg.lognoisevariance), ...
+        randn (ni, algo.nsamplepaths));
+else
+    noise_sim = [];
+end
+
+% Simulate samplepaths
+z_simc = stk_conditioning (lambda, zi, z_sim0, xi_ind, noise_sim);
+
+% Discrete distribution of the maximizer
+[~, ind_maximum] = max (z_simc((ni + 1):end, :));
+p = (hist (ind_maximum, 1:nc)) / algo.nsamplepaths;
+
+% Compute current entropy
+p_log_p = p .* (log (p));
+p_log_p(p < 1e-300) = 0;
+H_current = - sum (p_log_p);
+
+
 %% COMPUTE THE STEPWISE UNCERTAINTY REDUCTION CRITERION
+
+% We don't want any fancy object in here
+xi = double (xi);  zi = double (zi);
+xg = double (xg);  z_sim0 = double (z_sim0);
 
 % allocate sampling criterion vector
 CondH = zeros (nc, 1);
@@ -72,68 +101,67 @@ while ~CONDH_OK
             stk_disp_progress ('  ..', ic, nc);
         end
         
-        xi_ind(ni + 1) = ni + ic;
-        xi = xg(xi_ind, :);
+        % Index of candidate point in xg
+        ind_candi = ni + ic;
         
         % Noise variance
         lnv = get_lognoisevariance (algo.model, algo.xg0, ic, true);
         noisevariance = exp (lnv);
         
         % Heteroscedastic case: store lnv in model.lognoisevariance
+        % (because we called stk_fakenorep, model_.lognoisevariance is a vector)
         model_ = model_xg;
         model_.lognoisevariance = [model_.lognoisevariance; lnv];
         
-        if size(xi.data, 1) == size(unique(xi.data, 'rows'), 1) || noisevariance > 0.0
+        if (ismember (xg(ind_candi, :), xi, 'rows')) && (noisevariance == 0.0)
             
-            [~, lambda_] = stk_predict (model_, xi_ind, [], []);
+            % No change of entropy if we sample this point again
+            CondH(ic) = H_current;
             
-            zQ = stk_quadrature (1, algo, zp.mean(ni + ic), ...
-                zp.var(ni + ic) + noisevariance);
+        else
+            
+            % Compute prediction weight with ni + 1 points
+            [~, lambda_] = stk_predict (model_, [xi_ind; ind_candi], [], []);
+            
+            % Compute the part of zsimc that does not depend on k
+            delta = bsxfun (@minus, zi, z_sim0(xi_ind, :));
+            if (~ isempty (noise_sim))  % Noisy case ?
+                delta = delta - noise_sim;
+            end
+            z_simc = z_sim0 + lambda_(1:ni, :)' * delta;
+            
+            % Compute quadrature points
+            zQ = stk_quadrature (1, algo, zp.mean(ind_candi), ...
+                zp.var(ind_candi) + noisevariance);
             
             H = zeros(algo.Q,1);
             for k = 1:algo.Q
                 
-                zi_ = [zi; zQ(k)]; % add a fictitious observation
-                
-                % condition on the fictitious observation
-                zsimc = stk_conditioning(lambda_, zi_, zsim, xi_ind);
-                
-                [~, ind_maximum] = max(zsimc);
+                % Finish the computation of zsimc
+                delta = zQ(k) - z_sim0(ind_candi, :);
+                if noisevariance > 0
+                    delta = delta - (sqrt (noisevariance)) ...
+                        * (randn (1, algo.nsamplepaths));
+                end
+                z_simc_ = z_simc + lambda_(ni + 1, :)' * delta;
                 
                 % estimate the entropy of the maximizer distribution
-                
-                F = hist(ind_maximum, 1:ng);
-                p = F/algo.nsamplepaths;
-                
+                [~, ind_maximum] = max(z_simc_((ni + 1):end, :));
+                p = (hist (ind_maximum, 1:nc)) / algo.nsamplepaths;
                 p_log_p = p .* (log (p));
                 p_log_p(p < 1e-300) = 0;
-                
                 H(k) = - sum (p_log_p);
                 
             end
             
             CondH(ic) = stk_quadrature(2, algo, H);
             
-        else
-            
-            xi_ind = xi_ind(1:ni); % drop the test point
-            zsimc = stk_conditioning(lambda, zi, zsim, xi_ind);
-            [~, ind_maximum] = max (zsimc);
-            
-            F = hist(ind_maximum, 1:ng);
-            p = F/algo.nsamplepaths;
-            
-            p_log_p = p .* (log (p));
-            p_log_p(p < 1e-300) = 0;
-            
-            CondH(ic) = - sum (p_log_p);
-            
         end
     end % loop over candidate points
     
     CONDH_OK = true;
     
-    % 	if any( CondH > max(CondH(xi_ind(1:ni)) + 2e-2*(max(CondH) - min(CondH))) )
+    % 	if any( CondH > max(CondH(xi_ind) + 2e-2*(max(CondH) - min(CondH))) )
     % 		% keyboard
     % 		disp(['Warning: numerical instability detected' ...
     % 			  ' while computing entropy, increasing quadrature order']);
