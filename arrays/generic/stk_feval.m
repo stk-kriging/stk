f119b2fe8a55c2b41be8361f5991103c6aa234b8
Julien Bect <julien.bect@centralesupelec.fr>
2015-02-04 08:54:14 +0100

    stk_feval.m: Accept multivariate outputs (closes #20)
    * arrays/generic/stk_feval.m: Accept functions with multivariate
      outputs (i.e., outputs with several columns). Improve the
      construction of the output dataframe (column names). Add unit
      tests.

--- arrays/generic/stk_feval.m
+++ arrays/generic/stk_feval.m
@@ -57,7 +57,7 @@
 function z = stk_feval (f, x, progress_msg)
 
 if nargin > 3,
-   stk_error ('Too many input arguments.', 'TooManyInputArgs');
+    stk_error ('Too many input arguments.', 'TooManyInputArgs');
 end
 
 xdata = double (x);
@@ -69,17 +69,25 @@ if ~ iscell (f),  f = {f};  end
 numfcs = numel (f);
 
 % Check f and extract function names
-zname = cell (size (f));
-for j = 1:numfcs,
-    if ischar (f{j}),
-        zname{j} = f{j};
-    elseif ~ isa (f{j}, 'function_handle')
+fname = cell (size (f));
+truncated_fname = false (size (f));
+for k = 1:numfcs,
+    
+    if ischar (f{k}),
+        fname{k} = f{k};
+    elseif ~ isa (f{k}, 'function_handle')
         stk_error (['Argument ''f'' should be a cell-array of function ' ...
             'names or function handles.'], 'IncorrectType');
     else
-        zname{j} = func2str (f{j});
+        fname{k} = func2str (f{k});
     end
-end        
+    
+    % Truncate long function names
+    if (length (fname{k})) > 15
+        fname{k} = sprintf ('F%d', k);
+        truncated_fname(k) = true;
+    end
+end
 
 % Check 'progress_msg' argument
 if nargin < 3,
@@ -97,38 +105,110 @@ if d == 0,
     error ('zero-dimensional inputs are not allowed.');
 end
 
+% Each function can have several outputs => we will try to recover meaningful
+% column names by looking at these outputs and store them in:
+fcolnames = cell (1, numfcs);
+
 if n == 0, % no input => no output
     
     zdata = zeros (0, numfcs);
     
 else % at least one input point
     
-    zdata = zeros (n, numfcs);
+    %--- First evaluation: figure out the dimension of the output --------------
     
-    for i = 1:n,
-        if progress_msg,
-            stk_disp_progress ('feval %d/%d... ', i, n);
+    if progress_msg,
+        stk_disp_progress ('feval %d/%d... ', 1, n);
+    end
+    
+    zdata = cell (1, numfcs);
+    output_dim = zeros (1, numfcs);
+    
+    for k = 1:numfcs
+        
+        % Get the evaluation result
+        %  (at this point, we don't know the size or type of zdata{k})
+        zdata{k} = feval (f{k}, xdata(1, :));
+        
+        % Guess output dimension
+        output_dim(k) = size (zdata{k}, 2);
+        if ~ isequal (size (zdata{k}), [1 output_dim(k)])
+            stk_error (['The output of F{j} should be a scalar or a row ' ...
+                'vector'], 'IncorrectSize');
         end
-        for j = 1:numfcs
-            zdata(i, j) = feval (f{j}, xdata(i, :));
+        
+        % Guess column names
+        if isa (zdata{k}, 'stk_dataframe')
+            fcolnames{k} = zdata{k}.colnames;
+        end
+        
+        % Make sure that zdata{k} is of class 'double'
+        zdata{k} = double (zdata{k});
+    end
+    
+    zdata = cell2mat (zdata);
+    
+    % Begin/end indices for each block of columns
+    j_end = cumsum (output_dim);
+    j_beg = 1 + [0 j_end(1:end-1)];
+
+    if n > 1,  %--- Subsequent evaluations -------------------------------------
+               
+        for i = 2:n,
+            for k = 1:numfcs
+                zdata(i, (j_beg(k):j_end(k))) = feval (f{k}, xdata(i, :));
+            end
         end
     end
     
 end
 
-z = stk_dataframe (zdata, zname);
+%--- Create column names -------------------------------------------------------
+
+colnames = cell (1, size (zdata, 2));
+
+for k = 1:numfcs
+    
+    if ~ isempty (fcolnames{k})  % We have column names, let's use them
+                
+        % Special case: only one function, without a nice short displayable name
+        if (numfcs == 1) && truncated_fname(1)
+            prefix = '';
+        else
+            prefix = [fname{1} '_'];
+        end
+
+        colnames(j_beg(k):j_end(k)) = cellfun (...
+            @(u)(sprintf ('%s%s', prefix, u)), fcolnames{k}, ...
+            'UniformOutput', false);
+               
+    elseif output_dim(k) == 1  % Only one column and no column names
+        
+        colnames{j_beg(k)} = fname{k};
+        
+    else  % General case: several columns but no column names
+                
+        colnames(j_beg(k):j_end(k)) = arrayfun (...
+            @(u)(sprintf ('%s_%d', fname{k}, u)), 1:output_dim(k), ...
+            'UniformOutput', false);
+    end
+end
+
+%--- Create output stk_dataframe object ----------------------------------------
+
+z = stk_dataframe (zdata, colnames);
 z.info = 'Created by stk_feval';
 
 if isa (x, 'stk_dataframe'),
     z.rownames = x.rownames;
 end
-
+    
 end % function stk_feval
 
 
 %!shared f xt
-%!  f = @(x)(- (0.7 * x + sin (5 * x + 1) + 0.1 * sin (10 * x)));
-%!  xt = stk_sampling_regulargrid (20, 1, [0; 1]);
+%! f = @(x)(- (0.7 * x + sin (5 * x + 1) + 0.1 * sin (10 * x)));
+%! xt = stk_sampling_regulargrid (20, 1, [0; 1]);
 
 %!error  yt = stk_feval ();
 %!error  yt = stk_feval (f);
@@ -148,16 +228,31 @@ end % function stk_feval
 %! assert (isequal (y.data, [2; 4; 6]));
 %! assert (isequal (y.rownames, {'a'; 'b'; 'c'}));
 
+%!shared t z_ref n
+%! n = 20;
+%! t = stk_sampling_regulargrid (n, 1, [0; 2*pi]);
+%! z_ref = [sin(t.data) cos(t.data)];
+
 %!test
-%! t = stk_sampling_regulargrid (10, 1, [0; 2*pi]);
 %! t.colnames = {'time'};
 %! z = stk_feval ({@sin @cos}, t);
 %! assert (isa (z, 'stk_dataframe'));
-%! assert (all (size(z) == [10, 2]) );
+%! assert (isequal (z.data, z_ref));
 
 %!test
-%! t = stk_sampling_regulargrid (20, 1, [0; 2*pi]);
+%! F = @(x)([sin(x) cos(x)]);
+%! z = stk_feval (F, t);
+%! assert (isequal (z.data, z_ref));
+
+%!test
+%! t = stk_sampling_regulargrid (n, 1, [0; 2*pi]);
 %! F = {'sin' 'cos'};
 %! z = stk_feval (F, t);
 %! assert (isequal (z.data, [sin(t.data) cos(t.data)]));
 %! assert (isequal (z.colnames, {'sin' 'cos'}));
+
+%!test
+%! F = @(t)([sin(t) cos(t)]);
+%! G = @(t)(0.365 * t^2 + (cos ((t - 1)*(t - 2) + 0.579033)));
+%! z = stk_feval ({@sin @cos G F 'tan'}, t);
+%! assert (isequal (z.colnames, {'sin' 'cos' 'F3' 'F4_1' 'F4_2' 'tan'}));
