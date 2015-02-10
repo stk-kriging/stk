d7978e045466f7cea1368c98e21155d314d05e2e
Julien Bect <julien.bect@centralesupelec.fr>
2015-02-10 14:11:47 +0100

    Fix more backward compatibility issues
    * core/stk_predict.m: Convert xi to double (same reason).
    * paramestim/stk_param_estim.m: Convert zi to double (same reason).
    * arrays/@struct/stk_length.m: New function.

---
+++ arrays/@struct/stk_length.m
@@ -0,0 +1,38 @@
+% STK_LENGTH  [overload STK function]
+
+% Copyright Notice
+%
+%    Copyright (C) 2015 CentraleSupelec
+%
+%    Author:  Julien Bect  <julien.bect@supelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function l = stk_length (x)
+
+l = size (x.a, 1);
+
+end % function stk_length
+
+
+%!test
+%! x = struct ('a', [5; 2]);
+%! assert (isequal (stk_length (x), 2));
