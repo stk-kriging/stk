5635a433e36ce6fde13dad249bbda7c4a7eebf44
Julien Bect <julien.bect@centralesupelec.fr>
2015-05-27 10:02:49 +0200

    stk_dominatedhv_mex.c: Deal properly with less than 2 objectives

--- misc/pareto/stk_dominatedhv.m
+++ misc/pareto/stk_dominatedhv.m
@@ -1,13 +1,13 @@
 % STK_DOMINATEHV computes the hypervolume dominated by a set of points
 %
-% CALL: HV = stk_dominatedhv (Y_DATA, Y_REF)
+% CALL: HV = stk_dominatedhv (Y, Y_REF)
 %
-%    computes the hypervolume dominated by the rows of Y_DATA, with respect to
-%    the reference point Y_REF (a row vector with the same number of columns
-%    as Y_DATA). It is expected that all the rows of X are smaller than Y_REF
-%    (i.e., multi-objective minimization framework).
+%    computes the hypervolume dominated by the rows of Y, with respect to the
+%    reference point Y_REF (a row vector with the same number of columns as Y).
+%    It is expected that all the rows of Y_DATA are smaller than Y
+%    (multi-objective minimization framework).
 %
-% CALL: HV = stk_dominatedhv (Y_DATA)
+% CALL: HV = stk_dominatedhv (Y)
 %
 %    uses [0 0 ... 0] as a reference point.
 %
@@ -31,7 +31,6 @@
 % Copyright Notice
 %
 %    Copyright (C) 2015 CentraleSupelec
-%    Copyright (C) 2014 SUPELEC
 %
 %    Author:  Julien Bect  <julien.bect@centralesupelec.fr>
 
@@ -55,29 +54,29 @@
 %    You should  have received a copy  of the GNU  General Public License
 %    along with STK.  If not, see <http://www.gnu.org/licenses/>.
 
-function hv = stk_dominatedhv (y_data, y_ref)
+function hv = stk_dominatedhv (y, y_ref)
 
-y_data = double (y_data);
+y = double (y);
 
-if (~ ismatrix (y_data))
+if (~ ismatrix (y))
     stk_error ('y_data should be a matrix', 'IncorrectArgument');
 end
 
 if nargin < 2  % Use [0 0 ... 0] as a reference point
     
     % WFG convention: maximization problem
-    y_data = - y_data;
+    y = - y;
     
 elseif nargin == 2  % Reference point provided
     
     % Check the size of y_ref
-    if ~ isequal (size (y_ref), [1 size(y_data, 2)])
+    if ~ isequal (size (y_ref), [1 size(y, 2)])
         stk_error (['The number of columns of y_ref should be equal to ' ...
             'the number of columns of y_data'], 'IncorrectArgument');
     end
     
     % WFG convention: maximization problem
-    y_data = bsxfun (@minus, y_ref, y_data);
+    y = bsxfun (@minus, y_ref, y);
     
 else
     
@@ -85,23 +84,42 @@ else
     
 end
 
-if ~ all (y_data >= 0);
+if ~ all (y >= 0);
     stk_error ('All data points must be below the reference point', ...
         'IncorrectArgument');
 end
 
-hv = stk_dominatedhv_mex (y_data);
+hv = stk_dominatedhv_mex (y);
 
 end % function stk_dominatedhv
 
 
-%!test  % Example from README.TXT in WFg 1.10
+%!shared y, hv0 % Example from README.TXT in WFg 1.10
 %!
-%! y_data = [ ...
+%! y = [ ...
 %!     0.598 0.737 0.131 0.916 6.745; ...
 %!     0.263 0.740 0.449 0.753 6.964; ...
 %!     0.109 8.483 0.199 0.302 8.872 ];
 %!
-%! hv = stk_dominatedhv (- y_data);
-%!
-%! assert (stk_isequal_tolrel (hv, 1.1452351120, 1e-10));
+%! hv0 = 1.1452351120;
+
+%!error hv = stk_dominatedhv ();
+%!error hv = stk_dominatedhv (-y, [0 0 0 0 0], 'too many input args');
+
+%!test
+%! hv = stk_dominatedhv (-y);
+%! assert (stk_isequal_tolrel (hv, hv0, 1e-10));
+
+%!test
+%! hv = stk_dominatedhv (-y, [0 0 0 0 0]);
+%! assert (stk_isequal_tolrel (hv, hv0, 1e-10));
+
+%!test
+%! hv = stk_dominatedhv (1 - y, [1 1 1 1 1]);
+%! assert (stk_isequal_tolrel (hv, hv0, 1e-10));
+
+%!test
+%! for d = 1:10,
+%!    y = - 0.5 * ones (1, d);
+%!    assert (isequal (stk_dominatedhv (y), 0.5 ^ d));
+%! end
