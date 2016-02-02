59d1888031296ce3b4b6ec17514f37eac494e690
Julien Bect <julien.bect@centralesupelec.fr>
2016-02-02 20:35:21 +0100

    Add some basic support for LOO cross-validation
    * core/@stk_model_gpposterior/stk_predict_leaveoneout.m: New
      function that computes LOO predictions and residuals. Currently
      implemented in a very basic manner (virtual-LOO formulas will be
      implemented later).
    * core/stk_predict_leaveoneout.m: Just a wrapper around the first
      one (very inefficient as it is, since we create a "posterior" that
      we don't really use... but we should).
    * misc/plot/stk_plot_predvsobs.m: New graphical function, to
      visualize predictions versus observations.
    * misc/plot/stk_plot_histnormres.m: New graphical function, to the
      distribution of normalized residuals.
    * examples/01_kriging_basics/stk_example_kb10.m: Example script.
    * stk_example_kb10.m: Add new functions to the index.

---
+++ misc/plot/stk_plot_histnormres.m
@@ -0,0 +1,75 @@
+% STK_PLOT_HISTNORMRES plots an histogram for normalized residuals
+%
+% CALL: H = stk_plot_histnormres (NORM_RES, ...)
+%
+% CALL: H = stk_plot_histnormres (H_AXES, NORM_RES, ...)
+%
+% See also stk_predict_leaveoneout, stk_example_kb10
+
+% Copyright Notice
+%
+%    Copyright (C) 2016 CentraleSupelec
+%
+%    Author:  Julien Bect  <julien.bect@centralesupelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function h = stk_plot_histnormres (varargin)
+
+[h_axes, varargin, n_argin] = stk_plot_getaxesarg (varargin{:});
+
+if n_argin >= 1
+    norm_res = double (varargin{1});
+    opts = varargin(2:end);
+else
+    stk_error ('Not enough input arguments.', 'NotEnoughInputArgs');
+end
+
+% Choose the number of bins using the Freedman-Diaconis rule
+n = length (norm_res);
+q = quantile (norm_res, [0 0.25 0.75 1]);
+binsize = 2 * (q(3) -q(2)) * (n ^ (- 1/3));
+nbins = ceil ((q(4) - q(1)) / binsize);
+
+% Compute and plot histogram pdf
+[count, rr] = hist (norm_res, nbins);
+pdf = count / (n * (rr(2) - rr(1)));
+h.hist = bar (rr, pdf, 'hist');  hold on;
+
+% Center view
+M = max (3, max (xlim ()));  xlim ([-M, M]);
+
+% Plot reference N(0, 1) pdf
+rr = linspace (-M, M, 100);
+pdf_ref = 1 / (sqrt (2 * pi)) * (exp (- 0.5 * (rr .^ 2)));
+h.ref_pdf = plot (rr, pdf_ref, 'r--');
+
+% Apply options
+if ~ isempty (opts)
+    set (h.hist, opts{:});
+end
+
+% Create labels
+h_labels = stk_labels (h_axes, 'normalized residuals', 'probability density');
+h.xlabel = h_labels(1);
+h.ylabel = h_labels(2);
+
+end % function
