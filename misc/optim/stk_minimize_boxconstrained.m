0d3f255f30346f6baa3ba12b62a90c81c03504db
Julien Bect <julien.bect@centralesupelec.fr>
2016-02-04 16:51:28 +0100

    Provide generic stk_minimize_* functions
    * misc/optim/stk_minimize_boxconstrained.m: Generic
      stk_minimize_boxconstrained function, which accepts a char array
      as first input argument, and provides a generic documentation for
      all stk_minimize_boxconstrained methods.
    * misc/optim/stk_minimize_unconstrained.m: Same thing for
      stk_minimize_unconstrained methods.
    * admin/octpkg/INDEX: Add new functions to the index.

---
+++ misc/optim/stk_minimize_boxconstrained.m
@@ -0,0 +1,65 @@
+% STK_MINIMIZE_BOXCONSTRAINED performs box-constrained minimization
+%
+% CALL: U_OPT = stk_minimize_boxconstrained (ALGO, F, U_INIT, LB, UB)
+%
+%   minimizes the objective function F between LB and UB, using algorithm
+%   ALGO with starting point U_INIT.  The best point found by the algorithm is
+%   returned as U_OPT.  The value of ALGO can be 'fmincon', 'octavesqp', or any
+%   algorithm object implementing the 'stk_minimize_boxconstrained' method.
+%
+% CALL: [U_OPT, F_OPT] = stk_minimize_boxconstrained (ALGO, F, U_INIT, LB, UB)
+%
+%   also returns the best objective value F_OPT.
+%
+% See also stk_minimize_unconstrained, stk_optim_octavesqp, stk_optim_fmincon
+
+% Copyright Notice
+%
+%    Copyright (C) 2016 CentraleSupelec
+%
+%    Author:  Julien Bect  <julien.bect@centralesupelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function varargout = stk_minimize_boxconstrained (algo, varargin)
+
+switch algo
+    
+    case 'fmincon'
+        algo = stk_optim_fmincon ();
+        
+    case 'octavesqp'
+        algo = stk_optim_octavesqp ();
+        
+    otherwise        
+        if ~ ismethod (algo, 'stk_minimize_boxconstrained')            
+            stk_error (['The first input argument should be an algorithm ' ...
+                'object, implementing the stk_minimize_boxconstrained ', ...
+                'method.'], 'InvalidArgument');            
+        else            
+            stk_error ('Unexpected failure.', 'UnexpectedFailure');            
+        end
+end
+
+varargout = cell (1, max (1, nargout));
+[varargout{:}] = stk_minimize_boxconstrained (algo, varargin{:});
+
+end % function
