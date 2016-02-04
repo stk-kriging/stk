063741ee3bc7a8357464791ee581f46e739335bc
Julien Bect <julien.bect@centralesupelec.fr>
2016-02-04 16:02:25 +0100

    stk_optimscript.m: Fix %!shared blocks
    * iago/stk_optimscript.m: Fix %!shared blocks.

--- iago/stk_optimscript.m
+++ iago/stk_optimscript.m
@@ -137,13 +137,13 @@ options = [options {'pause', false}];
 [x_opt, f_opt, ~, aux] = stk_optim (f, DIM, BOX, xi, [], MAX_ITER, options);
 
 
-%!shared MAX_ITER TESTCASE_NUM NOISY
+%!shared MAX_ITER, TESTCASE_NUM, NOISY
 %! MAX_ITER = 2;  TESTCASE_NUM = 1;  NOISY = true;
 
 %!test  CRIT = 'IAGO';  stk_optimscript;
 %!error CRIT = 'EI';    stk_optimscript;  % EI cannot be used in the noisy case
 
-%!shared MAX_ITER TESTCASE_NUM CRIT
+%!shared MAX_ITER, TESTCASE_NUM, CRIT
 %! MAX_ITER = 2;  TESTCASE_NUM = 2;  CRIT = 'IAGO';
 
 %!test  NOISY = false; stk_optimscript;
