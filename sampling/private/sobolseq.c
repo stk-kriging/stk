bec4200521c36dd2a1454508cd43c3e6df1b6da5
Julien Bect <julien.bect@centralesupelec.fr>
2016-02-23 18:00:14 +0100

    sobolseq.c: Import useful lines from nlopt-util.h

--- sampling/private/sobolseq.c
+++ sampling/private/sobolseq.c
@@ -49,7 +49,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "nlopt-util.h"
+/* Sobol' low-discrepancy-sequence generation */
+typedef struct nlopt_soboldata_s *nlopt_sobol;
+extern nlopt_sobol nlopt_sobol_create(unsigned sdim);
+extern void nlopt_sobol_destroy(nlopt_sobol s);
+extern void nlopt_sobol_next01(nlopt_sobol s, double *x);
+extern void nlopt_sobol_next(nlopt_sobol s, double *x,
+        const double *lb, const double *ub);
+extern void nlopt_sobol_skip(nlopt_sobol s, unsigned n, double *x);
 
 #if defined(HAVE_STDINT_H)
 #  include <stdint.h>
