8b85dd7dbd4acd6d7244a50c7a0a9023858e297c
Julien Bect <julien.bect@centralesupelec.fr>
2016-03-08 14:24:26 +0100

    Make it possible to update heteroscedastic models
    * core/@stk_model_gpposterior/set_lognoisevariance.m: Provide a
      more informative error message when the size is wrong. Add unit
      tests.
    * core/@stk_model_gpposterior/stk_model_update.m: Handle the
      heteroscedastic case thanks to a new input argument. Provide
      informative error messages. Add unit tests.

--- core/@stk_model_gpposterior/set_lognoisevariance.m
+++ core/@stk_model_gpposterior/set_lognoisevariance.m
@@ -29,14 +29,19 @@
 function model = set_lognoisevariance (model, lnv, recompute)
 
 % Check lnv
-if ~ isscalar (lnv)    
+if ~ isscalar (lnv)
+    
+    % Number of observations
     n = size (model.input_data, 1);
-    if (~ isvector (lnv)) || (length (lnv) ~= n)
-        stk_error (['lnv must be either a scalar or a vector' ...
-            ' of length size (xi, 1).'], 'InvalidArgument');
-    end    
+        
+    if (~ isvector (lnv)) || (length (lnv) ~= n)        
+        stk_error (sprintf (['lnv must be either a scalar or a vector ' ...
+            'of length n=%d (the number of observations).\n\nHere, ' ...
+            'size (lnv) = [%s].'], n, num2str (size (lnv))), 'InvalidArgument');
+    end
+    
     % Make sure that lnv is a column vector
-    lnv = lnv(:);
+    lnv = reshape (lnv, n, 1);
 end
 
 model.prior_model.lognoisevariance = lnv;
@@ -45,5 +50,34 @@ model.prior_model.lognoisevariance = lnv;
 if (nargin < 3) || (recompute)
     model.kreq = stk_kreq_qr (model.prior_model, model.input_data);
 end
-    
+
 end % function
+
+
+%!shared x_obs, z_obs, ref, M_prior
+%! [x_obs, z_obs, ref] = stk_dataset_twobumps ('noisy2');
+%! M_prior = stk_model ('stk_materncov52_iso');
+%! M_prior.param = [-0.15; 0.38];
+%! M_prior.lognoisevariance = 2 * log (ref.noise_std);
+
+%!assert (~ isscalar (ref.noise_std));  % heteroscedastic dataset
+
+%!test  % homoscedastic case
+%! M_post = stk_model_gpposterior (M_prior, x_obs, z_obs);
+%! M_post.lognoisevariance = 0;  % scalar lnv, OK
+
+%!test  % heteroscedastic case
+%! M_post = stk_model_gpposterior (M_prior, x_obs, z_obs);
+%! M_post.lognoisevariance = 2 * log (ref.noise_std);
+
+%!error  % lnv vector too long
+%! M_post = stk_model_gpposterior (M_prior, x_obs, z_obs);
+%! x_new = [-0.79; -0.79];
+%! z_new = [-0.69; -0.85];
+%! lnv_new = ref.noise_std_func (x_new);
+%! M_post.lognoisevariance(end + (1:2)) = lnv_new;  % NOT OK
+
+%!error  % lnv vector too short
+%! M_post = stk_model_gpposterior (M_prior, x_obs, z_obs);
+%! M_post.lognoisevariance = ref.noise_std(1:end-1);  % NOT OK
+
