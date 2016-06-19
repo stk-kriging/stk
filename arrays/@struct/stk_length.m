cdf16335352b821947ddf144913bbe47816a7ce0
Julien Bect <julien.bect@centralesupelec.fr>
2016-06-19 10:24:40 +0200

    INDEX: Indicate more systematically which functions are considered internal

--- arrays/@struct/stk_length.m
+++ arrays/@struct/stk_length.m
@@ -1,8 +1,10 @@
-% STK_LENGTH [overload STK function]
+% STK_LENGTH [overload STK function, deprecated]
+%
+% DEPRECATION WARNING: The use of .a structures is deprecated.
 
 % Copyright Notice
 %
-%    Copyright (C) 2015 CentraleSupelec
+%    Copyright (C) 2015, 2016 CentraleSupelec
 %
 %    Author:  Julien Bect  <julien.bect@centralesupelec.fr>
 
