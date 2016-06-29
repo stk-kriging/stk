18f3a78b6e937af7f84e1864ac9be959caccf97f
Rémi Stroh <remi.stroh@lne.fr>
2016-06-29 17:59:18 +0200

    arrays/@stk_dataframe: Overload 25 additional unary base functions (sin, exp...)
    * arrays/@stk_dataframe/acos.m: New overloaded base function
    * arrays/@stk_dataframe/acosd.m: Idem.
    * arrays/@stk_dataframe/acosh.m: Idem.
    * arrays/@stk_dataframe/asin.m: Idem.
    * arrays/@stk_dataframe/asind.m: Idem.
    * arrays/@stk_dataframe/asinh.m: Idem.
    * arrays/@stk_dataframe/atan.m: Idem.
    * arrays/@stk_dataframe/atand.m: Idem.
    * arrays/@stk_dataframe/atanh.m: Idem.
    * arrays/@stk_dataframe/cos.m: Idem.
    * arrays/@stk_dataframe/cosd.m: Idem.
    * arrays/@stk_dataframe/cosh.m: Idem.
    * arrays/@stk_dataframe/exp.m: Idem.
    * arrays/@stk_dataframe/expm1.m: Idem.
    * arrays/@stk_dataframe/log.m: Idem.
    * arrays/@stk_dataframe/log10.m: Idem.
    * arrays/@stk_dataframe/log1p.m: Idem.
    * arrays/@stk_dataframe/log2.m: Idem.
    * arrays/@stk_dataframe/sin.m: Idem.
    * arrays/@stk_dataframe/sind.m: Idem.
    * arrays/@stk_dataframe/sinh.m: Idem.
    * arrays/@stk_dataframe/sqrt.m: Idem.
    * arrays/@stk_dataframe/tan.m: Idem.
    * arrays/@stk_dataframe/tand.m: Idem.
    * arrays/@stk_dataframe/tanh.m: Idem.
    * admin/octpkg/INDEX: Add new functions to the INDEX

---
+++ arrays/@stk_dataframe/asin.m
@@ -0,0 +1,38 @@
+% ASIN [overload base function]
+
+% Copyright Notice
+%
+%    Copyright (C) 2014 SUPELEC
+%
+%    Author: Julien Bect  <julien.bect@centralesupelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function ydata = asin (x)
+
+ydata = asin (x.data);
+
+end % function
+
+
+%!test
+%! u = rand (4, 3);  x = stk_dataframe (u);  v = asin (x);
+%! assert (strcmp (class (v), class (u)) && isequal (v, asin (u)))
