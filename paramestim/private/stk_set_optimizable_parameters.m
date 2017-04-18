9b5b6afa000accd92d2acd00f6434ac4a24f783c
Julien Bect <julien.bect@centralesupelec.fr>
2017-04-18 22:37:02 +0200

    Rewrite two private functions with try-catch blocks instead of tests
    * paramestim/private/stk_get_optimizable_parameters.m: Use a
      try-catch block instead of testing what the input is.
    * paramestim/private/stk_set_optimizable_parameters.m: Idem.

--- paramestim/private/stk_set_optimizable_parameters.m
+++ paramestim/private/stk_set_optimizable_parameters.m
@@ -51,10 +51,9 @@ function param = stk_set_optimizable_parameters (param, value)
 % neither param nor value is an object of a "parameter class" (more precisely,
 % a class that implements stk_set_optimizable_parameters)
 
-if isnumeric (param) ...
-        || (isobject (param) && ismethod (param, 'subsasgn')) % Backward compat.
+try
     
-    % If param is numeric, we preserve its size and type of param:
+    % If param is numeric, the following syntax preserves its size and type
     param(:) = value;
     
     % Note: if param is an object, the previous line is actually a call to
@@ -62,8 +61,8 @@ if isnumeric (param) ...
     % introduced in STK 2.0.0 as an "experimental" feature. It is now
     % deprecated.
     
-else
-    
+catch
+        
     stk_error (['stk_set_optimizable_parameters is not implemented for ' ...
         'objects of class ', class(param), '.'], 'TypeMismatch');
     
