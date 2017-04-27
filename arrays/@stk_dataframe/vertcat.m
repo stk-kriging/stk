a38ce9a05e61abd67b5fb9e4ff434f042ffe957e
Julien Bect <jbect@users.sourceforge.net>
2017-04-27 22:01:55 +0200

    vertcat.m: Source formatting

--- arrays/@stk_dataframe/vertcat.m
+++ arrays/@stk_dataframe/vertcat.m
@@ -29,7 +29,7 @@
 
 function x = vertcat (x, y, varargin)
 
-if nargin < 2,
+if nargin < 2
     y = [];
 end
 
@@ -74,7 +74,7 @@ else
     x_empty = cellfun (@isempty, x_colnames);
     y_empty = cellfun (@isempty, y_colnames);
     xy_equal = strcmp (x_colnames, y_colnames);
-
+    
     if all (x_empty | y_empty | xy_equal)
         colnames = x_colnames;
         colnames(x_empty) = y_colnames(x_empty);
@@ -88,14 +88,14 @@ end
 
 %--- Create output row names ----------------------------------------------
 
-bx = isempty(x_rownames);
-by = isempty(y_rownames);
+bx = isempty (x_rownames);
+by = isempty (y_rownames);
 
-if bx && by, % none of the argument has row names
+if bx && by  % none of the argument has row names
     
     rownames = {};
     
-else % at least of one the arguments has row names
+else  % at least of one the arguments has row names
     
     if bx,  x_rownames = repmat ({''}, size (x_data, 1), 1);  end
     if by,  y_rownames = repmat ({''}, size (y_data, 1), 1);  end
@@ -115,7 +115,7 @@ else
     x = stk_dataframe (data, colnames, rownames);
 end
 
-if ~ isempty (varargin),
+if ~ isempty (varargin)
     x = vertcat (x, varargin{:});
 end
 
@@ -124,37 +124,37 @@ end % function
 %#ok<*SPWRN>
 
 
-% IMPORTANT NOTE: [x; y; ...] fails to give the same result as vertcat(x, y,
+% IMPORTANT NOTE: [x; y; ...] fails to give the same result as vertcat (x, y,
 % ...) in some releases of Octave. As a consequence, all tests must be written
-% using horzcat explicitely.
+% using vertcat explicitely.
 
 %!shared u, v
-%! u = rand(3, 2);
-%! v = rand(3, 2);
+%! u = rand (3, 2);
+%! v = rand (3, 2);
 
 %%
 % Vertical concatenation of two dataframes
 
 %!test
-%! x = stk_dataframe(u);
-%! y = stk_dataframe(v);
+%! x = stk_dataframe (u);
+%! y = stk_dataframe (v);
 %! z = vertcat (x, y);
-%! assert (isa(z, 'stk_dataframe') && isequal(double(z), [u; v]));
+%! assert (isa (z, 'stk_dataframe') && isequal (double (z), [u; v]));
 
-%!test % the same, with row names this time
-%! x = stk_dataframe(u, {}, {'a'; 'b'; 'c'});
-%! y = stk_dataframe(v, {}, {'d'; 'e'; 'f'});
+%!test  % the same, with row names this time
+%! x = stk_dataframe (u, {}, {'a'; 'b'; 'c'});
+%! y = stk_dataframe (v, {}, {'d'; 'e'; 'f'});
 %! z = vertcat (x, y);
-%! assert (isa(z, 'stk_dataframe') && isequal(double(z), [u; v]));
-%! assert (all(strcmp(z.rownames, {'a'; 'b'; 'c'; 'd'; 'e'; 'f'})));
+%! assert (isa (z, 'stk_dataframe') && isequal (double (z), [u; v]));
+%! assert (all (strcmp (z.rownames, {'a'; 'b'; 'c'; 'd'; 'e'; 'f'})));
 
-%!test % the same, with row names only for the first argument
-%! x = stk_dataframe(u, {}, {'a'; 'b'; 'c'});
-%! y = stk_dataframe(v);
+%!test  % the same, with row names only for the first argument
+%! x = stk_dataframe (u, {}, {'a'; 'b'; 'c'});
+%! y = stk_dataframe (v);
 %! z = vertcat (x, y);
-%! assert (isa(z, 'stk_dataframe') && isequal(double(z), [u; v]));
+%! assert (isa (z, 'stk_dataframe') && isequal (double (z), [u; v]));
 
-%!test % incompatible variable names
+%!test  % incompatible variable names
 %! u = rand (3, 1);  x = stk_dataframe (u, {'x'});
 %! v = rand (3, 1);  y = stk_dataframe (v, {'y'});
 %! z = vertcat (x, y);
@@ -164,14 +164,14 @@ end % function
 % Vertical concatenation [dataframe; matrix]
 
 %!test
-%! x = stk_dataframe(u);
+%! x = stk_dataframe (u);
 %! z = vertcat (x, v);
-%! assert (isa(z, 'stk_dataframe') && isequal(double(z), [u; v]));
+%! assert (isa (z, 'stk_dataframe') && isequal (double (z), [u; v]));
 
-%!test % the same, with row names for the first argument
-%! x = stk_dataframe(u, {}, {'a'; 'b'; 'c'});
+%!test  % the same, with row names for the first argument
+%! x = stk_dataframe (u, {}, {'a'; 'b'; 'c'});
 %! z = vertcat (x, v);
-%! assert (isa(z, 'stk_dataframe') && isequal(double(z), [u; v]));
+%! assert (isa (z, 'stk_dataframe') && isequal (double (z), [u; v]));
 
 %%
 % Vertical concatenation [matrix; dataframe]
@@ -185,10 +185,10 @@ end % function
 % Vertical concatenation of more than two elements
 
 %!test
-%! x = stk_dataframe(u);
-%! y = stk_dataframe(v);
+%! x = stk_dataframe (u);
+%! y = stk_dataframe (v);
 %! z = vertcat (x, y, u, v);
-%! assert(isa(z, 'stk_dataframe') && isequal(double(z), [u; v; u; v]));
+%! assert (isa (z, 'stk_dataframe') && isequal (double (z), [u; v; u; v]));
 
 %%
 % Vertical concatenation with missing column names
@@ -202,6 +202,6 @@ end % function
 %! z = [x; y];
 %! assert (isequal (z.colnames, {'a' 'b' 'c'}))
 
-%!test 
+%!test
 %! z = [y; x];
 %! assert (isequal (z.colnames, {'a' 'b' 'c'}))
