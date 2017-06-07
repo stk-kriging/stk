01f715f93f971c1e9c0825ce3b1dd24ccaee4bbf
Julien Bect <jbect@users.sourceforge.net>
2017-06-07 22:17:59 +0200

    Optimize for speed
    * core/@stk_kreq_qr/get.m: Use a simple switch.
    * core/@stk_model_gpposterior/stk_predict.m: Call get directly.
    * sampling/@stk_sampcrit_akg/feval.m: Call get directly.

--- core/@stk_kreq_qr/get.m
+++ core/@stk_kreq_qr/get.m
@@ -6,7 +6,7 @@
 
 % Copyright Notice
 %
-%    Copyright (C) 2015 CentraleSupelec
+%    Copyright (C) 2015, 2017 CentraleSupelec
 %    Copyright (C) 2013, 2014 SUPELEC
 %
 %    Author:  Julien Bect       <julien.bect@centralesupelec.fr>
@@ -34,84 +34,76 @@
 
 function value = get (kreq, propname)
 
-try
+switch propname
     
-    % if it's a field in the underlying structure, don't waste time
-    value = kreq.(propname);
-    
-catch
-    
-    switch propname
+    case {'n', 'r', 'P_scaling', 'LS_Q', 'LS_R', 'RS', 'lambda_mu'}
+        value = kreq.(propname);
         
-        case 'lambda'
-            value = kreq.lambda_mu(1:kreq.n, :);
-            
-        case 'mu'
-            value = kreq.lambda_mu((kreq.n + 1):end, :);
-            
-        case 'delta_var'
-            value = dot (kreq.lambda_mu, kreq.RS, 1)';
-            
-        case 'log_abs_det_kriging_matrix'
-            % LS_Q has +/- 1 determinant, so we only need to care about LS_R
-            value = sum (log (abs (diag (kreq.LS_R))));
-            
-        case 'log_det_covariance_matrix_a'
-            n = kreq.n;
-            Q11 = kreq.LS_Q(1:n, 1:n);
-            R11 = kreq.LS_R(1:n, 1:n);
-            value = log (det (Q11 * R11)); % K = Q11 * R11
-            % note: this method seems much less accurate than the others,
-            %       we strongly recommend to avoid using it
-            
-        case 'log_det_covariance_matrix_b'
-            n = kreq.n;
-            Q11 = kreq.LS_Q(1:n, 1:n);
-            R11 = kreq.LS_R(1:n, 1:n);
-            Kchol = stk_cholcov (Q11 * R11); % K = Q11 * R11
-            value = 2 * sum (log (diag (Kchol)));
-            
-        case 'log_det_covariance_matrix_c'
-            % note: very efficient (but not the best) and quite simple to understand
-            n = kreq.n;
-            Q11 = kreq.LS_Q(1:n, 1:n);
-            diag_R  = diag (kreq.LS_R);
-            logdet1 = log (abs (det (Q11)));
-            logdet2 = sum (log (abs (diag_R(1:n))));
-            value = logdet1 + logdet2;
-            
-        case {'log_det_covariance_matrix', 'log_det_covariance_matrix_d'}
-            % note: this is the fastest of all five solutions for large matrices
-            %       (for small matrices, the difference is negligible)... but also
-            %       the trickiest !
-            n = kreq.n;
-            QL = kreq.LS_Q((n + 1):end, :); % lowerpart of LS_Q
-            diag_R = diag (kreq.LS_R);
-            logdet1 = sum (log (abs (diag_R)));
-            T = kreq.LS_R \ (QL');
-            logdet2 = log (abs (det (T((n + 1):end, :))));
-            value = logdet1 + logdet2;
-            
-        case 'log_det_covariance_matrix_e'
-            % note: the most time consuming of all five solutions (blame qrdelete),
-            %       we strongly recommend to avoid using it
-            n = kreq.n;
-            r = kreq.r;
-            Q = kreq.LS_Q;
-            R = kreq.LS_R;
-            for i = 0:(r - 1),
-                [Q, R] = qrdelete (Q, R, n + r - i, 'row');
-                [Q, R] = qrdelete (Q, R, n + r - i, 'col');
-            end
-            value = sum (log (abs (diag (R))));
-            
-        otherwise
-            error ('There is no property called ''%s''.', propname);
-            
-    end % switch
-    
-end % try_catch
+    case 'lambda'
+        value = kreq.lambda_mu(1:kreq.n, :);
+        
+    case 'mu'
+        value = kreq.lambda_mu((kreq.n + 1):end, :);
+        
+    case 'delta_var'
+        value = dot (kreq.lambda_mu, kreq.RS, 1)';
+        
+    case 'log_abs_det_kriging_matrix'
+        % LS_Q has +/- 1 determinant, so we only need to care about LS_R
+        value = sum (log (abs (diag (kreq.LS_R))));
+        
+    case 'log_det_covariance_matrix_a'
+        n = kreq.n;
+        Q11 = kreq.LS_Q(1:n, 1:n);
+        R11 = kreq.LS_R(1:n, 1:n);
+        value = log (det (Q11 * R11)); % K = Q11 * R11
+        % note: this method seems much less accurate than the others,
+        %       we strongly recommend to avoid using it
+        
+    case 'log_det_covariance_matrix_b'
+        n = kreq.n;
+        Q11 = kreq.LS_Q(1:n, 1:n);
+        R11 = kreq.LS_R(1:n, 1:n);
+        Kchol = stk_cholcov (Q11 * R11); % K = Q11 * R11
+        value = 2 * sum (log (diag (Kchol)));
+        
+    case 'log_det_covariance_matrix_c'
+        % note: very efficient (but not the best) and quite simple to understand
+        n = kreq.n;
+        Q11 = kreq.LS_Q(1:n, 1:n);
+        diag_R  = diag (kreq.LS_R);
+        logdet1 = log (abs (det (Q11)));
+        logdet2 = sum (log (abs (diag_R(1:n))));
+        value = logdet1 + logdet2;
+        
+    case {'log_det_covariance_matrix', 'log_det_covariance_matrix_d'}
+        % note: this is the fastest of all five solutions for large matrices
+        %       (for small matrices, the difference is negligible)... but also
+        %       the trickiest !
+        n = kreq.n;
+        QL = kreq.LS_Q((n + 1):end, :); % lowerpart of LS_Q
+        diag_R = diag (kreq.LS_R);
+        logdet1 = sum (log (abs (diag_R)));
+        T = kreq.LS_R \ (QL');
+        logdet2 = log (abs (det (T((n + 1):end, :))));
+        value = logdet1 + logdet2;
+        
+    case 'log_det_covariance_matrix_e'
+        % note: the most time consuming of all five solutions (blame qrdelete),
+        %       we strongly recommend to avoid using it
+        n = kreq.n;
+        r = kreq.r;
+        Q = kreq.LS_Q;
+        R = kreq.LS_R;
+        for i = 0:(r - 1)
+            [Q, R] = qrdelete (Q, R, n + r - i, 'row');
+            [Q, R] = qrdelete (Q, R, n + r - i, 'col');
+        end
+        value = sum (log (abs (diag (R))));
+        
+    otherwise
+        error ('There is no property called ''%s''.', propname);
+        
+end % switch
 
 end % function
-
-%#ok<*CTCH>
