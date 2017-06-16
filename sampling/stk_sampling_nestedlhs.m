6e25b5acf7d4092e4a21a15edd3fcf32ffc12489
Julien Bect <jbect@users.sourceforge.net>
2017-06-16 11:30:57 +0200

    Don't use ~ to ignore output arguments (see CG#07)
    * admin/CODING_GUIDELINES: Improve CG#07
    * sampling/stk_sampcrit_akg_eval.m: Apply CG#07
    * sampling/stk_sampling_nesteddesign.m: Apply CG#07
    * sampling/stk_sampling_nestedlhs.m: Apply CG#07

--- sampling/stk_sampling_nestedlhs.m
+++ sampling/stk_sampling_nestedlhs.m
@@ -185,7 +185,7 @@ for k_lev = nLev:-1:1; %begin by the end
         
         X_new_kl = NaN(n_new_k, dim);    %the matrix to add
         
-        [~, random_index] = sort(rand(n_new_k, dim), 1);
+        [ignd, random_index] = sort(rand(n_new_k, dim), 1);  % See CG#07
         for i = 1:dim
             X_new_kl(:, i) = list_nb_new_k(random_index(:, i), i);
         end
