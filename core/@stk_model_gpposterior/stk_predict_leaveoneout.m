d7f3fd6d74b478988bc357fc38c155f04181808e
Julien Bect <jbect@users.sourceforge.net>
2017-07-24 10:22:48 +0200

    @stk_model_gpposterior/stk_predict_leaveoneout.m: Source formatting

--- core/@stk_model_gpposterior/stk_predict_leaveoneout.m
+++ core/@stk_model_gpposterior/stk_predict_leaveoneout.m
@@ -5,9 +5,8 @@
 %    Copyright (C) 2017 CentraleSupelec
 %    Copyright (C) 2017 LNE
 %
-%    Author:  Julien Bect      <julien.bect@centralesupelec.fr>
-%             Stefano Duhamel  <stefano.duhamel@supelec.fr>
-%             Remi Stroh       <remi.stroh@lne.fr>
+%    Authors:  Remi Stroh   <remi.stroh@lne.fr>
+%              Julien Bect  <julien.bect@centralesupelec.fr>
 
 % Copying Permission Statement
 %
@@ -31,47 +30,46 @@
 
 function [LOO_pred, LOO_res] = stk_predict_leaveoneout (M_post)
 
-if nargin > 1,
+if nargin > 1
     stk_error ('Too many input arguments.', 'TooManyInputArgs');
 end
 
 prior_model = M_post.prior_model;
 
 % Compute the covariance matrix, and the trend matrix
-[K, P] = stk_make_matcov(prior_model, M_post.input_data);
-% Remark: the covariance K takes into account of the noise
-% (covmat_response)
+% (this covariance matrix K takes the noise into account)
+[K, P] = stk_make_matcov (prior_model, M_post.input_data);
 simple_kriging = (size (P, 2) == 0);
 
 % If simple kriging, just compute the inverse covariance matrix
 if simple_kriging
-    R = inv(K);
+    R = inv (K);
 else
     % Use a more complex formula ("virtual cross-validation")
-    P_K = P'/K;
-    R = K\(eye(size(K)) - P*((P_K*P)\P_K));
-    % I = inv(K);
-    % R = I - I*P*inv(P'*I*P)*P'*I;
+    P_K = P' / K;
+    R = K \ (eye (size (K)) - P * ((P_K * P) \ P_K));
+    % I = inv (K);
+    % R = I - I * P * (inv (P' * I * P)) * P' * I;
 end
-dR = diag(R);   % The diagonal of the LOO matrix
+dR = diag (R);  % The diagonal of the LOO matrix
 
 % Mean
 zi = M_post.output_data;
-raw_res = R*zi./dR;     % Compute "raw" residuals
-zp_mean = zi - raw_res; % LOO-prediction
+raw_res = R * zi ./ dR;  % Compute "raw" residuals
+zp_mean = zi - raw_res;  % LOO prediction
 
 % Error
 noisevariance = exp (M_post.prior_model.lognoisevariance);
-zp_var = 1./dR - noisevariance; % Do not forget the noise!
+zp_var = 1./dR - noisevariance;  % Do not forget the noise!
 
-LOO_pred = stk_dataframe([zp_mean, zp_var], {'mean', 'var'});
+LOO_pred = stk_dataframe ([zp_mean, zp_var], {'mean', 'var'});
 
 % Compute residuals ?
 if nargout ~= 1
     
     % Compute normalized residual
     % norm_res = (zi - zp_mean) ./ (sqrt (noisevariance + zp_var));
-    norm_res = sqrt(dR).*raw_res;
+    norm_res = (sqrt (dR)) .* raw_res;
     
     % Pack results into a dataframe
     LOO_res = stk_dataframe ([raw_res, norm_res], ...
@@ -80,13 +78,13 @@ end
 
 % Create LOO cross-validation plots?
 if nargout == 0
-
+    
     % Plot predictions VS observations (left planel)...
     stk_subplot (1, 2, 1);  stk_plot_predvsobs (M_post.output_data, LOO_pred);
     
     % ...and normalized residuals (right panel)
     stk_subplot (1, 2, 2);   stk_plot_histnormres (LOO_res.norm_res);
-
+    
 end
 
 end % function
