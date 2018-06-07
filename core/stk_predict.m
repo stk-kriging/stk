f8447e91e90e3b8cde0c60b2d1a6cee8c728930a
Julien Bect <jbect@users.sourceforge.net>
2018-06-07 08:37:51 +0200

    Check for duplicated rows when ill-conditioning is detected (ticket #74)
    * misc/error/stk_assert_no_duplicates.m: New function.
    * admin/octpkg/INDEX: Add new function to the index.
    * core/@stk_kreq_qr/stk_kreq_qr.m: Check for duplicated rows when
      ill-conditioning is detected.
    * param/estim/stk_param_relik.m: Idem.
    * core/stk_predict.m: Add unit test.
    * core/@stk_model_gpposterior/stk_model_gpposterior.m: Source
      formatting.

--- core/stk_predict.m
+++ core/stk_predict.m
@@ -93,14 +93,15 @@ end % function
 %! z_obs = sin (double (x_obs));
 %! x_prd = x0(idx_prd);
 %!
-%! model = stk_model('stk_materncov32_iso');
+%! model = stk_model ('stk_materncov32_iso');
 %! model.param = log ([1.0; 2.1]);
 
-%!error y_prd1 = stk_predict();
-%!error y_prd1 = stk_predict(model);
-%!error y_prd1 = stk_predict(model, x_obs);
-%!error y_prd1 = stk_predict(model, x_obs, z_obs);
-%!test  y_prd1 = stk_predict(model, x_obs, z_obs, x_prd);
+%!error y_prd1 = stk_predict ();
+%!error y_prd1 = stk_predict (model);
+%!error y_prd1 = stk_predict (model, x_obs);
+%!error y_prd1 = stk_predict (model, x_obs, z_obs);
+%!test  y_prd1 = stk_predict (model, x_obs, z_obs, x_prd);
+%!error y_prd1 = stk_predict (model, [x_obs; x_obs], [z_obs; z_obs], x_prd);
 
 %!test  % nargout = 2
 %! [y_prd1, lambda] = stk_predict (model, x_obs, z_obs, x_prd);
