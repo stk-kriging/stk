53e0cd5c756e9e8455a0ba267d6c1d6bfce63c97
Julien Bect <julien.bect@centralesupelec.fr>
2018-07-11 20:23:30 +0200

    Implement stk_set/get_optimizable_parameters for models
    * core/@stk_model_gpposterior/stk_set_optimizable_parameters.m:
      Implement stk_set_optimizable_parameters for posterior models.
    * core/@struct/stk_set_optimizable_parameters.m: Implement
      stk_set_optimizable_parameters for prior models, currently
      described by a structure.
    * core/@stk_model_gpposterior/stk_get_optimizable_parameters.m:
      Implement stk_get_optimizable_parameters for posterior models.
    * core/@struct/stk_get_optimizable_parameters.m: Implement
      stk_get_optimizable_parameters for prior models, currently
      described by a structure.

---
+++ core/@struct/stk_set_optimizable_parameters.m
@@ -0,0 +1,70 @@
+% STK_SET_OPTIMIZABLE_PARAMETERS [overload STK function, internal]
+%
+% INTERNAL FUNCTION WARNING:
+%
+%    This function is currently considered as internal.  STK users that wish to
+%    experiment with parameter classes can already overload it, but should be
+%    aware that API-breaking changes are likely to happen in future releases.
+%
+% See also: stk_set_optimizable_parameters
+
+% Copyright Notice
+%
+%    Copyright (C) 2017, 2018 CentraleSupelec
+%    Copyright (C) 2017 LNE
+%
+%    Authors:  Remi Stroh   <remi.stroh@lne.fr>
+%              Julien Bect  <julien.bect@centralesupelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function model = stk_set_optimizable_parameters (model, value)
+
+assert_struct_is_model (model);
+
+% Covariance parameters
+covparam = stk_get_optimizable_parameters (model.param);
+covparam_size = length (covparam);
+
+% Noise parameters
+noiseparam = stk_get_optimizable_noise_parameters (model);
+noiseparam_size = length (noiseparam);
+
+% Check length of value
+if numel (value) ~= (covparam_size + noiseparam_size)
+    stk_error (['The length of ''value'' must be equal to ' ...
+        'the number of parameters.'], 'IncorrectSize');
+end
+
+% Change covariance parameter
+if covparam_size > 0
+    covparam = value(1:covparam_size);
+    model.param = stk_set_optimizable_parameters (model.param, covparam);
+end
+
+% Change noise variance parameter
+if noiseparam_size > 0
+    noiseparam = value(covparam_size + (1:noiseparam_size));
+    model.lognoisevariance = stk_set_optimizable_parameters ...
+        (model.lognoisevariance, noiseparam);
+end
+
+end % function
