3befecf04d402ddc9e0c982eaaf2a5771a211e90
Julien Bect <julien.bect@centralesupelec.fr>
2018-07-12 15:03:40 +0200

    stk_param_estim.m: Remove get_default_bounds_lnv subfunction (ooops)

--- param/estim/stk_param_estim.m
+++ param/estim/stk_param_estim.m
@@ -215,25 +215,6 @@ end
 end % function
 
 
-function [lblnv,ublnv] = get_default_bounds_lnv ... % --------------------------
-    (model, lnv0, xi, zi) %#ok<INUSL>
-
-TOLVAR = 0.5;
-
-% Bounds for the variance parameter
-empirical_variance = var(zi);
-lblnv = log (eps);
-ublnv = log (empirical_variance) + TOLVAR;
-
-% Make sure that lnv0 falls within the bounds
-if ~ isempty (lnv0)
-    lblnv = min (lblnv, lnv0 - TOLVAR);
-    ublnv = max (ublnv, lnv0 + TOLVAR);
-end
-
-end % function
-
-
 function [param0, lnv0] = provide_param0_value ... % ---------------------------
     (model, xi, zi, param0, lnv0)
 
