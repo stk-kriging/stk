3f6580edd22b8d99d314a21415a0b9a3fd21bb5a
Julien Bect <julien.bect@centralesupelec.fr>
2018-07-14 07:36:53 +0200

    Encapsulate Gaussian logpdf / logpdf_grad computations
    * misc/distrib/stk_distrib_logpdf.m: Compute the Gaussian logpdf,
      still assuming the good ol' struct representation.
    * misc/distrib/stk_distrib_logpdf_grad.m: Compute the gradient of
      the Gaussian logpdf.
    * param/estim/stk_param_relik.m: Simplify the code using the two
      new internal functions.

---
+++ misc/distrib/stk_distrib_logpdf_grad.m
@@ -0,0 +1,55 @@
+% STK_DISTRIB_LOGPDF_GRAD [STK internal]
+%
+% Trying to make things cleaner, until we finally develop an elegant system of
+% probability distribution objects...
+%
+% INTERNAL FUNCTION WARNING:
+%
+%    This function is currently considered as internal.  Please be aware that
+%    API-breaking changes are likely to happen in future releases.
+
+% Copyright Notice
+%
+%    Copyright (C) 2016, 2018 CentraleSupelec
+%    Copyright (C) 2016 LNE
+%
+%    Authors:  Julien Bect  <julien.bect@centralesupelec.fr>
+%              Remi Stroh   <remi.stroh@lne.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function dlogpdf = stk_distrib_logpdf_grad (distrib, z)
+
+delta = z - distrib.mean;
+
+if isfield (distrib, 'invcov')
+    
+    % We assume (but do not check) that .var is absent or compatible with invcov...
+    
+    dlogpdf = - distrib.invcov * delta;
+    
+else  % assume isfield (distrib, 'var')
+    
+    dlogpdf = - distrib.var \ delta;
+    
+end
+
+end % function
