ffaf3600b7f72e9b86806795fe2b7da8e4e6040c
Julien Bect <jbect@users.sourceforge.net>
2019-08-08 15:31:46 +0200

    @stk_model_base: Remove some cruft in comments

--- model/@stk_model_base/get.m
+++ model/@stk_model_base/get.m
@@ -1,10 +1,4 @@
 % GET [overload base function]
-%
-% EXPERIMENTAL CLASS WARNING:  The stk_model_gn class is currently considered
-%    experimental.  STK users who wish to experiment with it are welcome to do
-%    so, but should be aware that API-breaking changes are likely to happen in
-%    future releases.  We invite them to direct any questions, remarks or
-%    comments about this experimental class to the STK mailing list.
 
 % Copyright Notice
 %
