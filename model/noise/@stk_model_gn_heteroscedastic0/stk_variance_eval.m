216569a1cfce1c70400a5e9d21a4b9e23d70419f
Julien Bect <julien.bect@centralesupelec.fr>
2019-08-12 15:04:40 +0200

    Mark @stk_model_gn_heteroscedastic0/stk_variance_eval.m as experimental
    * model/noise/@stk_model_gn_heteroscedastic0/stk_variance_eval.m:
      Mark this new function as experimental.
    * admin/octpkg/INDEX: update index.

--- model/noise/@stk_model_gn_heteroscedastic0/stk_variance_eval.m
+++ model/noise/@stk_model_gn_heteroscedastic0/stk_variance_eval.m
@@ -1,4 +1,29 @@
-% FEVAL [overload base function]
+% STK_VARIANCE_EVAL [experimental]
+%
+% CALL: V = stk_variance_eval (NOISEMODEL, X, DIFF)
+%
+% EXPERIMENTAL FUNCTION WARNING
+%
+%   This function is currently considered experimental, since the whole
+%   'noise model classes' thing is.
+%
+%   Classes derived from stk_model_gn must implement this to define the
+%   value of the variance at any given point x.
+%
+%   In the future, it might be a good idea to make such a function part of
+%   the "standard API" of all models in STK, together with similar
+%   functions such as:
+%
+%    * M = stk_mean_eval (MODEL, X, DIFF)
+%    * V = stk_variance_eval (NOISEMODEL, X, DIFF)
+%    * Q = stk_quantile_eval (NOISEMODEL, X, DIFF)
+%    * ...
+%
+%    STK users that wish to experiment with this function, and more
+%    generally with noise model objects, are welcome to do so, but should
+%    be aware that the API is very likely to change in the future.
+%    Please send questions, comments and suggestions about this part of the
+%    toolbox to the STK mailing list.
 
 % Copyright Notice
 %
