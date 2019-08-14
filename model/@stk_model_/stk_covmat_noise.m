ba00cd4520cd51f712649f322b1081c63524f177
Julien Bect <jbect@users.sourceforge.net>
2019-08-14 14:53:28 +0200

    Implement stk_covmat_noise for all models
    * core/stk_covmat_noise.m: Move from
    * model/prior_struct/stk_covmat_noise.m: Move to
    * core/@stk_model_gpposterior/stk_covmat_noise.m: New function
    * model/@stk_model_/stk_covmat_noise.m: New function
    * model/noise/@stk_gaussiannoise_/stk_covmat_noise.m: New function
    * admin/octpkg/INDEX: Update

---
+++ model/@stk_model_/stk_covmat_noise.m
@@ -0,0 +1,37 @@
+% STK_COVMAT_NOISE [STK internal]
+
+% Copyright Notice
+%
+%    Copyright (C) 2019 CentraleSupelec
+%
+%    Author:  Julien Bect  <julien.bect@centralesupelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function [K, P1, P2] = stk_covmat_noise (model, x1, x2, diff, pairwise)
+
+stk_error (['Classes derived from stk_model_ must implement ' ...
+    'stk_covmat_noise.'], 'IncompleteClassImplementation');
+
+
+end % function
+
+%#ok<*INUSD,*STOUT>
