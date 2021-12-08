f2289423c6390a5e827f9152c88292f9b20e5f18
Julien Bect <julien.bect@centralesupelec.fr>
2021-12-08 22:26:39 +0100

    github: Run linter
    * admin/stk_mlint_all.m: A function to run the linter on
      all the M-files of the toolbox.
    * .github/workflows/run-linter.yml: Github action.

---
+++ admin/stk_mlint_all.m
@@ -0,0 +1,87 @@
+% STK_MLINT_ALL
+
+% Copyright Notice
+%
+%    Copyright (C) 2021 CentraleSupelec
+%
+%    Author:  Julien Bect  <julien.bect@centralesupelec.fr>
+
+% Copying Permission Statement
+%
+%    This file is part of
+%
+%            STK: a Small (Matlab/Octave) Toolbox for Kriging
+%               (http://sourceforge.net/projects/kriging)
+%
+%    STK is free software: you can redistribute it and/or modify it under
+%    the terms of the GNU General Public License as published by the Free
+%    Software Foundation,  either version 3  of the License, or  (at your
+%    option) any later version.
+%
+%    STK is distributed  in the hope that it will  be useful, but WITHOUT
+%    ANY WARRANTY;  without even the implied  warranty of MERCHANTABILITY
+%    or FITNESS  FOR A  PARTICULAR PURPOSE.  See  the GNU  General Public
+%    License for more details.
+%
+%    You should  have received a copy  of the GNU  General Public License
+%    along with STK.  If not, see <http://www.gnu.org/licenses/>.
+
+function m = stk_mlint_all (root)
+
+if nargin == 0
+    root = fileparts (fileparts (mfilename ('fullpath')));
+end
+
+m = stk_mlint_all_ (root);
+
+fprintf ('\n\nSYMMARY:\n')
+[msg, ~, ic] = unique ({m.id});
+for k = 1:(length (msg))
+    fprintf ('% 3d %s\n', sum (ic == k), msg{k});
+end
+
+end % function
+
+
+function m = stk_mlint_all_ (root)
+
+m = [];
+
+s = dir (root);
+for i = 1:(length (s))
+
+    fn = fullfile (root, s(i).name);
+
+    if s(i).isdir  % Process subdirectories recursively
+        if s(i).name(1) == '.'
+            fprintf ('Skipping directory %s\n', fn);
+            continue
+        end % if
+
+        fprintf ('Processing directory %s ...\n', fn);
+        m_ = stk_mlint_all_ (fn);
+        fprintf ('Directory %s processed: %d messages\n', fn, length (m_));
+
+    else  % Process M-files
+
+        if (length (s(i).name) < 2) ...
+                || (~ strcmp (s(i).name(end-1:end), '.m'))
+            fprintf ('Skipping file %s\n', fn);
+            continue
+        end % if
+
+        fprintf ('Processing file %s ...\n', fn);
+        m_ = checkcode (fn, '-struct', '-id');
+        [m_.file] = deal (fn);
+
+    end % if
+
+    if isempty (m)
+        m = m_;
+    else
+        m = [m; m_];  %#ok<AGROW>
+    end
+
+end % for
+
+end % function
